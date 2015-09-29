c3836b3212e4dae109d3d342a62c392dc4277dc8
Christian Schafmeister <meister@temple.edu>
2015-09-28 21:14:18 -0400

    fixed genericFunction.cc

--- src/core/genericFunction.cc
+++ src/core/genericFunction.cc
@@ -298,9 +298,6 @@ generic_function_dispatch_vararg(cl_narg narg, ...)
 }
  */
 LCC_RETURN generic_function_dispatch(Instance_sp gf, VaList_sp vargs) {
-#ifdef DEBUG_ON
-//  _lisp->stack_monitor();
-#endif
   gc::tagged_pointer<Cache> cache(_lisp->methodCachePtr());
   return standard_dispatch(gf,vargs,cache);
 }
