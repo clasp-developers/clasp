c20f5c12b493865b920cd9749534d5cae7582686
drmeister <chris.schaf@verizon.net>
2016-05-18 23:41:24 -0400

    Fixed and ran setup-cclasp-build.lisp

--- src/lisp/kernel/cleavir/setup-cclasp-build.lisp
+++ src/lisp/kernel/cleavir/setup-cclasp-build.lisp
@@ -9,9 +9,12 @@
 
 
 (export '(load-system link))
+(format t "Loading asdf~%")
 (load "sys:modules;asdf;build;asdf.lisp")
+(format t "loading local-asdf-config.lisp~%")
 (load "sys:local-asdf-config.lisp")
 
+(format t "loading sys:kernel;cleavir;asdf-system-groveler.lisp~%")
 (load "sys:kernel;cleavir;asdf-system-groveler.lisp")
 
 (defun save-partial-system (filename system)
@@ -31,6 +34,6 @@
                             ;; auto-compile must preceed inline because the Cleavir compiler
                             ;; needs to be the default compiler before inlining is used to
                             ;; replace CL functions like CONSP, CAR, CDR, RPLACA etc
-                            #P"sys:kernel/cleavir/auto-compile"
-                            #P"sys:kernel/cleavir/inline")))
+                            #P"kernel/cleavir/auto-compile"
+                            #P"kernel/cleavir/inline")))
 (format t "Done~%")
