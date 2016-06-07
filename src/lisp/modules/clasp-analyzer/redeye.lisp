4117bde33a0d21cc7b035b517c0a66731960cf3e
drmeister <chris.schaf@verizon.net>
2016-06-07 11:30:22 -0400

    Added redeye

--- src/lisp/modules/clasp-analyzer/redeye.lisp
+++ src/lisp/modules/clasp-analyzer/redeye.lisp
@@ -1,21 +1,27 @@
 
+(unless (= (length core:*command-line-arguments*) 1 )
+  (format t "You must provide the path to the compilation database~%")
+  (sys:quit))
+
 (defparameter *compilation-database-file* (elt core:*command-line-arguments* 0))
 
-(load (compile-file #P"sys:modules;clang-tool;clang-tool.lsp"
+(format t "Loading clang-tool~%")
+(load (compile-file #P"sys:modules;clang-tool;clang-tool.lisp"
                     :output-file
                     (ensure-directories-exist
                      (translate-logical-pathname
                       (make-pathname
                        :host "tmp"
-                       :defaults (compile-file-pathname #P"sys:modules;clang-tool;clang-tool.lsp"))))))
+                       :defaults (compile-file-pathname #P"sys:modules;clang-tool;clang-tool.lisp"))))))
 
-(load (compile-file #P"sys:modules;clasp-analyzer;clasp-analyzer.lsp"
+(format t "Loading clasp-analyzer~%")
+(load (compile-file #P"sys:modules;clasp-analyzer;clasp-analyzer.lisp"
                     :output-file
                     (ensure-directories-exist
                      (translate-logical-pathname
                       (make-pathname
                        :host "tmp"
-                       :defaults (compile-file-pathname #P"sys:modules;clasp-analyzer;clasp-analyzer.lsp"))))))
+                       :defaults (compile-file-pathname #P"sys:modules;clasp-analyzer;clasp-analyzer.lisp"))))))
 
 (defparameter *compilation-database* (clasp-analyzer:setup-clasp-analyzer-compilation-tool-database *compilation-database-file*))
 (clasp-analyzer:search/generate-code *compilation-database*)
