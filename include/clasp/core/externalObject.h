c2b8348b017c714c4f77ee271d2310d25cf788d1
Frank Goenninger <frank.goenninger@goenninger.net>
2016-06-24 21:24:02 +0200

    CHANGED: Style adaptations (curly braces now after a statement). CHANGED: ForeignIObject_O instances now get initialized after having been GC_ALLOCATEd.

--- include/clasp/core/externalObject.h
+++ include/clasp/core/externalObject.h
@@ -1,37 +1,77 @@
-/*
-    File: externalObject.h
-*/
+// -*- Mode: C++; tab-width: 2; indent-tabs-mode: c++; c-basic-offset: 2 -*-
+//
+//   ██████╗    ██╗          █████╗     ███████╗    ██████╗
+//  ██╔════╝    ██║         ██╔══██╗    ██╔════╝    ██╔══██╗
+//  ██║         ██║         ███████║    ███████╗    ██████╔╝
+//  ██║         ██║         ██╔══██║    ╚════██║    ██╔═══╝
+//  ╚██████╗    ███████╗    ██║  ██║    ███████║    ██║
+//   ╚═════╝    ╚══════╝    ╚═╝  ╚═╝    ╚══════╝    ╚═╝
+//
+// ===========================================================================
+//    C L A S P - COMMON LISP AND C++
+// ===========================================================================
+//
+// Copyright (c) 2014, Christian E. Schafmeister
+//
+// CLASP is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Library General Public
+// License as published by the Free Software Foundation; either
+// version 2 of the License, or (at your option) any later version.
+//
+// See directory 'clasp/licenses' for full details.
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+// ===========================================================================
 
-/*
-Copyright (c) 2014, Christian E. Schafmeister
- 
-CLASP is free software; you can redistribute it and/or
-modify it under the terms of the GNU Library General Public
-License as published by the Free Software Foundation; either
-version 2 of the License, or (at your option) any later version.
- 
-See directory 'clasp/licenses' for full details.
- 
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
+//! @file   externalObject.h
+//! @author Christian E. Schafmeister
+//! @brief
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-*/
-/* -^- */
-#ifndef ExternalObject_H
-#define ExternalObject_H
+// -^-
 
-#include <clasp/core/foundation.h>
-#include <clasp/core/object.h>
-#include <clasp/core/standardObject.h>
+
+#if !defined( __CLASP_CORE_EXTERNAL_OBJECT_H__ )
+#define __CLASP_CORE_EXTERNAL_OBJECT_H__
+
+// ---------------------------------------------------------------------------
+//    SYSTEM INCLUDES
+// ---------------------------------------------------------------------------
+
+// --- Standard C++ Includes ---
+// NONE
+
+// --- Platform-specific Includes ---
+// NONE
+
+// ---------------------------------------------------------------------------
+//    APPLICaATION INCLUDES
+// ---------------------------------------------------------------------------
+
+#include "clasp/core/foundation.h"
+#include "clasp/core/object.h"
+#include "clasp/core/standardObject.h"
+
+#if defined( __cplusplus )
+
+// ---------------------------------------------------------------------------
+//   NAMESPACE
+// ---------------------------------------------------------------------------
 
 namespace core {
+
+// ---------------------------------------------------------------------------
+//   CLASSES & METHODS & FUNCTIONS
+// ---------------------------------------------------------------------------
 
 // set this class up by hand
   SMART(ExternalObject);
@@ -65,8 +105,6 @@ namespace core {
     wrapper->set_wrapped(ptr);
     return wrapper;
   }
-
-
 };
 
 // public:
@@ -74,61 +112,61 @@ namespace core {
 #ifndef SCRAPING
 #define LISP_EXTERNAL_CLASS(oNamespace, oPackage, wrappedClass, aClass, nameOfWrappedClass, aClassBase) \
   public:                                                               \
-    typedef aClassBase Base;                                            \
+  typedef aClassBase Base;                                            \
   typedef LispBases1<Base> Bases;                                       \
   COMMON_CLASS_PARTS(oNamespace, oPackage, aClass, nameOfWrappedClass)  \
   static gctools::smart_ptr<aClass> create() {                          \
-      return gctools::GC<aClass>::allocate_with_default_constructor();  \
-    };                                                                  \
+    return gctools::GC<aClass>::allocate_with_default_constructor();  \
+  };                                                                  \
   typedef wrappedClass WrappedClass;                                    \
- public:                                                                \
-  /*Derived from StandardObject so it supports slots*/                  \
-  static bool static_supportsSlots() { return true; };                  \
-  virtual core::Class_sp __class() const {                              \
-    return aClass::static_class;                                        \
-  }
+public:                                                                \
+ /*Derived from StandardObject so it supports slots*/                  \
+ static bool static_supportsSlots() { return true; };                  \
+ virtual core::Class_sp __class() const {                              \
+   return aClass::static_class;                                        \
+ }
   /* end */
 
 #endif // SCRAPING
 
 namespace core {
 
-typedef enum { DeleteOnDtor = 1,
-               Copyable = 2,
-} ForeignDataFlagEnum;
+  typedef enum { DeleteOnDtor = 1,
+                 Copyable = 2,
+  } ForeignDataFlagEnum;
 
 // set this class up by hand
-SMART(ForeignData);
+  SMART(ForeignData);
 /* Maintain a pointer to a block of Foreign data that we may or may not own depending on _OwnershipFlags */
 
-class ForeignData_O : public ExternalObject_O
-{
-  LISP_CLASS(core, CorePkg, ForeignData_O, "ForeignData",ExternalObject_O);
+  class ForeignData_O : public ExternalObject_O
+  {
+    LISP_CLASS(core, CorePkg, ForeignData_O, "ForeignData",ExternalObject_O);
 #if defined(XML_ARCHIVE)
-  void archiveBase(ArchiveP node);
+    void archiveBase(ArchiveP node);
 #endif // defined(XML_ARCHIVE)
        //	string	__repr__() const;
-  T_sp _Kind;
-  int _OwnershipFlags;
-  size_t _Size;
-  void *_Data;
+    T_sp _Kind;
+    int _OwnershipFlags;
+    size_t _Size;
+    void *_Data;
 
-public:
-  static ForeignData_sp allocateForeignObject(T_sp kind);
+  public:
+    static ForeignData_sp allocateForeignObject(T_sp kind);
 
-public:
-  template <class T>
-  T data() { return reinterpret_cast<T>(this->_Data); };
+  public:
+    template <class T>
+      T data() { return reinterpret_cast<T>(this->_Data); };
 
-private:
-  void allocate(T_sp kind, int ownershipFlags, size_t size);
+  private:
+    void allocate(T_sp kind, int ownershipFlags, size_t size);
 
-  void freeForeignObject();
+    void freeForeignObject();
 
-public:
-  explicit ForeignData_O();
-  virtual ~ForeignData_O(); // non-trivial
-};
+  public:
+    explicit ForeignData_O();
+    virtual ~ForeignData_O(); // non-trivial
+  };
 };
 
 
@@ -139,4 +177,9 @@ struct gctools::GCInfo<core::ForeignData_O> {
   static GCInfo_policy constexpr Policy = normal;
 };
 
-#endif
+// ---------------------------------------------------------------------------
+//   END OF FILE
+// ---------------------------------------------------------------------------
+
+#endif // __cplusplus
+#endif // __CLASP_CORE_EXTERNAL_OBJECT_H__
--- src/core/pointer.cc
+++ src/core/pointer.cc
@@ -1,62 +1,178 @@
-/*
-    File: pointer.cc
-*/
+// -*- Mode: C++; tab-width: 2; indent-tabs-mode: c++; c-basic-offset: 2 -*-
+//
+//   ██████╗    ██╗          █████╗     ███████╗    ██████╗
+//  ██╔════╝    ██║         ██╔══██╗    ██╔════╝    ██╔══██╗
+//  ██║         ██║         ███████║    ███████╗    ██████╔╝
+//  ██║         ██║         ██╔══██║    ╚════██║    ██╔═══╝
+//  ╚██████╗    ███████╗    ██║  ██║    ███████║    ██║
+//   ╚═════╝    ╚══════╝    ╚═╝  ╚═╝    ╚══════╝    ╚═╝
+//
+// ===========================================================================
+//    C L A S P - COMMON LISP AND C++
+// ===========================================================================
+//
+// Copyright (c) 2014, Christian E. Schafmeister
+//
+// CLASP is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Library General Public
+// License as published by the Free Software Foundation; either
+// version 2 of the License, or (at your option) any later version.
+//
+// See directory 'clasp/licenses' for full details.
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+// ===========================================================================
 
-/*
-Copyright (c) 2014, Christian E. Schafmeister
- 
-CLASP is free software; you can redistribute it and/or
-modify it under the terms of the GNU Library General Public
-License as published by the Free Software Foundation; either
-version 2 of the License, or (at your option) any later version.
- 
-See directory 'clasp/licenses' for full details.
- 
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
+//! @file   pointer.cc
+//! @author Christian E. Schafmeister
+//! @brief  Pointer Lisp Object Implementation
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-*/
-/* -^- */
-#include <clasp/core/common.h>
-#include <clasp/core/pointer.h>
-#include <clasp/core/wrappers.h>
+// -^-
+#if defined( __cplusplus )
+
+// ---------------------------------------------------------------------------
+//    SYSTEM INCLUDES
+// ---------------------------------------------------------------------------
+
+// --- Standard C++ Includes ---
+#include <iostream>
+
+// --- Platform-specific Includes ---
+// NONE
+
+// ---------------------------------------------------------------------------
+//    APPLICaATION INCLUDES
+// ---------------------------------------------------------------------------
+
+#include "clasp/core/common.h"
+#include "clasp/core/pointer.h"
+#include "clasp/core/wrappers.h"
+
+// ---------------------------------------------------------------------------
+//   NAMESPACE
+// ---------------------------------------------------------------------------
 
 namespace core {
 
-void Pointer_O::initialize() {
+// ---------------------------------------------------------------------------
+//   SPECIAL / GLOBAL DEFINES AND VARIABLES
+// ---------------------------------------------------------------------------
+
+// ---------------------------------------------------------------------------
+//   METHODS & FUNCTIONS
+// ---------------------------------------------------------------------------
+
+// ---------------------------------------------------------------------------
+//   @method  Pointer_O::initialize
+//   @brief   Initlalize Pointer_o instance
+// ---------------------------------------------------------------------------
+void Pointer_O::initialize( void ) {
   this->Base::initialize();
-  this->_Pointer = NULL;
+  this->m_rawptr = nullptr;
 }
 
-Pointer_sp Pointer_O::create(void *p) {
-  GC_ALLOCATE(Pointer_O, ptr);
-  ptr->_Pointer = p;
-  return ptr;
+// ---------------------------------------------------------------------------
+//   @method  Pointer_O :: create
+//   @brief   Create Pointer_O instance
+// ---------------------------------------------------------------------------
+Pointer_sp Pointer_O::create( void *ptr ) {
+  GC_ALLOCATE( Pointer_O, self );
+	self->initialize();
+  self->m_rawptr = ptr;
+  return self;
 }
 
+// ---------------------------------------------------------------------------
+//   @method Pointer_O::null_pointer()
+//   @brief  Make a nullptr
+// ---------------------------------------------------------------------------
+CL_PKG_NAME( CorePkg, null-pointer );
+CL_DEFUN Pointer_sp Pointer_O::null_pointer( void ) {
+  return Pointer_O::create( nullptr );
+}
 
+// ---------------------------------------------------------------------------
+//   @method Pointer_O::inc_pointer
+//   @brief  Increase foreign pointer address by offset
+// ---------------------------------------------------------------------------
+CL_LISPIFY_NAME( "core:inc-pointer" );
+CL_DEFMETHOD Pointer_sp Pointer_O::inc_pointer( core::Integer_sp offset ) {
+  void *new_ptr = (void *)((char *)( this->m_rawptr ) + clasp_to_int( offset ));
+  return Pointer_O::create( new_ptr );
+}
 
-
-
-
-bool Pointer_O::eql_(T_sp obj) const {
-  if (this->eq(obj)) return true;
-  if (Pointer_sp pobj = obj.asOrNull<Pointer_O>()) {
-    return (this->_Pointer == pobj->_Pointer);
+// ---------------------------------------------------------------------------
+//   @method  Pointer_O :: eql_
+//   @brief   EQL for Pointer_O
+// ---------------------------------------------------------------------------
+bool Pointer_O::eql_( T_sp obj ) const {
+  if ( this->eq( obj )) {
+    return true;
   }
-  return false;
+	else {
+		Pointer_sp p_obj;
+
+		p_obj = obj.asOrNull< Pointer_O >();
+
+		if ( p_obj ) {
+			return ( this->m_rawptr == p_obj->m_rawptr );
+		}
+	}
+
+	return false;
 }
 
+// ---------------------------------------------------------------------------
+//   @method  Pointer_O :: __repr__
+//   @brief   Print string representation of Pointer_O
+// ---------------------------------------------------------------------------
 string Pointer_O::__repr__() const {
   stringstream ss;
-  ss << "#<" << this->_instanceClass()->classNameAsString() << " :ptr " << (BF("%p") % this->_Pointer).str() << ">";
+
+  ss << "#<"
+     << this->_instanceClass()->classNameAsString()
+     << " :ptr "
+     << ( BF( "0x%x" ) % this->m_rawptr )
+     << ">";
+
   return ss.str();
 }
+
+// ---------------------------------------------------------------------------
+//   @method Pointer_O::make()
+//   @brief  Makes a pointer
+// ---------------------------------------------------------------------------
+CL_PKG_NAME( CorePkg, make-pointer );
+CL_DEFUN Pointer_sp Pointer_O::make( core::Number_sp arg ) {
+  if ( core__fixnump( arg )) {
+    if ( sizeof( unbox_fixnum( gc::As<core::Fixnum_sp>( arg))) != sizeof( void *)) {
+      SIMPLE_ERROR( BF( "You cannot make a pointer using an integer as the address sizeof(void*)=%d sizeof(Fixnum)=%d" ) % sizeof(void *) % sizeof(unbox_fixnum(gc::As<core::Fixnum_sp>(arg))));
+    }
+
+    IMPLEMENT_MEF( BF( "Deal with converting Fixnum or Bignum to void*" ));
+
+#if 0
+    return Pointer_O::create( (void*) ( arg.as< core::Fixnum_O >()->get()) );
+#endif
+  }
+
+  SIMPLE_ERROR( BF( "Illegal type for address of pointer" ));
 };
+
+// ---------------------------------------------------------------------------
+//   END OF FILE
+// ---------------------------------------------------------------------------
+
+}; // namespace
+
+#endif // __cplusplus
