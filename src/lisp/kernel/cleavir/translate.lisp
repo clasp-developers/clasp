30c74782be9514b6e13546d15d2922d7466168f7
drmeister <chris.schaf@verizon.net>
2016-07-15 23:50:46 -0700

    Removed more verbose compiler messages

--- src/lisp/kernel/cleavir/translate.lisp
+++ src/lisp/kernel/cleavir/translate.lisp
@@ -1042,12 +1042,12 @@ nil)
         ((cleavir-env:no-variable-info
           (lambda (condition)
 ;;;	  (declare (ignore condition))
-            (warn "Condition: ~a" condition)
+            #+verbose-compiler(warn "Condition: ~a" condition)
             (invoke-restart 'cleavir-generate-ast::consider-special)))
          (cleavir-env:no-function-info
           (lambda (condition)
 ;;;	  (declare (ignore condition))
-            (warn "Condition: ~a" condition)
+            #+verbose-compiler(warn "Condition: ~a" condition)
             (invoke-restart 'cleavir-generate-ast::consider-global))))
       (multiple-value-bind (fn function-kind wrapped-env lambda-name warnp failp)
           ;; The following test and true form should be removed`
