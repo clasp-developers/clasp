a22793f069dc57af57de4f69cd3491794a3a00d2
drmeister <chris.schaf@verizon.net>
2017-02-23 11:40:11 -0500

    Added file to start swank in docker

---
+++ src/lisp/modules/docker-swank/start-swank.lisp
@@ -0,0 +1,10 @@
+(format t "Starting swank~%")
+(load "/root/slime/swank-loader.lisp")
+(swank-loader:init
+ :delete nil
+ :reload nil
+ :load-contribs nil)
+(let ((swank::*loopback-interface* "0.0.0.0"))
+  (swank:create-server :port 4005
+		       :dont-close nil))
+(format t "You should never reach here~%")
