40525984c9fc6aa2bdf8e35e574cf22987a3ace3
Christian Schafmeister <meister@temple.edu>
2017-04-11 05:27:52 +0000

    Added sigthreadmask macro on linux

--- src/core/primitives.cc
+++ src/core/primitives.cc
@@ -100,6 +100,10 @@ THE SOFTWARE.
 #define sigthreadmask(HOW,NEW,OLD) sigprocmask((HOW),(NEW),(OLD))
 #endif
 
+#if defined( _TARGET_OS_LINUX )
+#define sigthreadmask(HOW,NEW,OLD) sigprocmask((HOW),(NEW),(OLD))
+#endif
+
 
 namespace core {
 
