dea5e93826825b54ec71f809b5e07e66e3b2a4eb
Ubuntu <ubuntu@ip-172-31-46-66.us-east-2.compute.internal>
2017-06-18 13:53:56 +0000

    working on cxxanalysis with docker

--- src/lisp/modules/clasp-analyzer/run-from-docker.lisp
+++ src/lisp/modules/clasp-analyzer/run-from-docker.lisp
@@ -1,8 +1,8 @@
-(load (compile-file "/home/app/mount/src/lisp/modules/clang-tool/clang-tool.lisp"
+(load (compile-file "/home/ubuntu/Dev/cando/src/lisp/modules/clang-tool/clang-tool.lisp"
                     :output-file "/home/app/mount-out/src/lisp/modules/clang-tool.fasl" :print t))
-(load (compile-file "/home/app/mount/src/lisp/modules/clasp-analyzer/clasp-analyzer.lisp"
+(load (compile-file "/home/ubuntu/Dev/cando/src/lisp/modules/clasp-analyzer/clasp-analyzer.lisp"
                     :output-file "/home/app/mount-out/src/lisp/modules/clasp-analyzer.fasl" :print t))
-(defparameter *compile-commands* "/home/app/mount/build/mpsprep/compile_commands.json")
+(defparameter *compile-commands* "/home/ubuntu/Dev/cando/build/mpsprep/compile_commands.json")
 (time (clasp-analyzer:search/generate-code (clasp-analyzer:setup-clasp-analyzer-compilation-tool-database (pathname *compile-commands*))
                                            :output-file "/home/app/mount-out/clasp_gc.cc"))
 (core:quit)
