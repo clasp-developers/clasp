2e9e88739f851cdcff36f4af05e0c5b09cb483ea
Bike <aeshtaer@gmail.com>
2017-07-03 14:43:37 -0400

    Make COPY-SYMBOL check boundedness

--- src/core/symbol.cc
+++ src/core/symbol.cc
@@ -231,8 +231,10 @@ CL_DEFMETHOD Symbol_sp Symbol_O::copy_symbol(T_sp copy_properties) const {
   Symbol_sp new_symbol = Symbol_O::create(this->_Name);
   if (copy_properties.isTrue()) {
     ASSERT(this->_Function);
-    new_symbol->_GlobalValue = this->symbolValue();
-    new_symbol->_Function = this->_Function;
+    if (sym->boundP())
+      new_symbol->_GlobalValue = this->symbolValue();
+    if (sym->fboundp())
+      new_symbol->_Function = this->_Function;
     new_symbol->_IsConstant = this->_IsConstant;
     new_symbol->_ReadOnlyFunction = this->_ReadOnlyFunction;
     new_symbol->_PropertyList = cl__copy_list(this->_PropertyList);
