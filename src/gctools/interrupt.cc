6c873010a04b0c2d28adafb10787565d089416fe
drmeister <chris.schaf@verizon.net>
2017-10-04 16:19:28 -0400

    Changed a stupid error message

--- src/gctools/interrupt.cc
+++ src/gctools/interrupt.cc
@@ -320,9 +320,9 @@ CL_DEFUN void core__check_pending_interrupts() {
 
 
 void fatal_error_handler(void *user_data, const std::string &reason, bool gen_crash_diag) {
-  printf("Hit a fatal error in llvm: %s\n", reason.c_str());
-  printf("I (clasp) shall sleep for 1000 seconds and await the sweet kiss of the debugger.\n  If it does not reach me in time - I shall perish by abort().\n");
-  printf("    Find me at pid %d before I die!\n", getpid());
+  printf("%s:%d Hit a fatal error in llvm: %s\n", __FILE__, __LINE__, reason.c_str());
+  printf("Clasp is sleeping for 1000 seconds in case you want to connect in with the debugger - after which it will abort().\n");
+  printf("    Clasp pid -> %d\n", getpid());
   sleep(1000);
   abort();
 }
