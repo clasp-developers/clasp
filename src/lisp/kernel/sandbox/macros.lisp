393d274f5b354f076be097c1ffb1c94573cc49e8
Bike <aeshtaer@gmail.com>
2017-10-12 14:57:52 -0400

    Sandbox: Move m-v-c def out of macros file

--- src/lisp/kernel/sandbox/macros.lisp
+++ src/lisp/kernel/sandbox/macros.lisp
@@ -1,7 +1,3 @@
-;;; definition works anywhere, probably- not implementation specific
-(defmacro multiple-value-call (fdesignator &rest args)
-  `(cleavir-primop:multiple-value-call (coerce-fdesignator fdesignator) ,@args))
-
 (defmacro defun (name lambda-list &body body)
   `(progn
      (setf (fdefinition ',name)
