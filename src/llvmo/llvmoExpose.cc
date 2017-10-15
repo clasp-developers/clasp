ecfabc782b176657aa1f29625fa57b1014fe2f50
drmeister <chris.schaf@verizon.net>
2017-10-15 13:32:28 -0400

    Change some comments

--- src/llvmo/llvmoExpose.cc
+++ src/llvmo/llvmoExpose.cc
@@ -3695,16 +3695,6 @@ https://groups.google.com/forum/#!topic/llvm-dev/m3JjMNswgcU
                                          ),
                            GDBEventListener(JITEventListener::createGDBRegistrationListener())
 {
-#if 0
-  RTDyldObjectLinkingLayer::NotifyLoadedFtor notify_func = [this](llvm::orc::RTDyldObjectLinkingLayer::ObjHandleT H,
-                                                                  const RTDyldObjectLinkingLayer::ObjectPtr& Obj,
-                                                                  const RuntimeDyld::LoadedObjectInfo &Info) {
-//          printf("%s:%d:%s informing GDBEventListener  i=%d &Objects[i]->%p  &Infos[i]->%p\n", __FILE__, __LINE__, __FUNCTION__, i, Objects[i].get(), Infos[i].get());
-      this->GDBEventListener->NotifyObjectEmitted(*(Obj->getBinary()), Info);
-      save_symbol_info(*(Obj->getBinary()), Info);
-  };
-  this->ObjectLayer.NotifyLoadedFtor = notify_func;
-#endif
   llvm::sys::DynamicLibrary::LoadLibraryPermanently(nullptr);
 }
 
@@ -3917,6 +3907,7 @@ CL_DEFUN void llvm_sys__removeAlwaysInlineFunctions(llvm::Module* module)
 
 std::shared_ptr<llvm::Module> optimizeModule(std::shared_ptr<llvm::Module> M) {
 #if 0
+  // An attempt to move optimization into Common Lisp
   Module_sp om = Module_O::create();
   om->set_wrapped(&*M);
   om = core::eval::funcall(comp::_sym_optimize_module_for_compile,om);
@@ -3924,7 +3915,7 @@ std::shared_ptr<llvm::Module> optimizeModule(std::shared_ptr<llvm::Module> M) {
   printf("%s:%d  Returning module\n", __FILE__, __LINE__ );
   return result;
 #else
-  
+  // Optimize in C++
   core::LightTimer timer;
   timer.start();
   // Create a function pass manager.
