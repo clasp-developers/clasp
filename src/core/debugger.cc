265992431075c7e018ae43f2eb7dbf89aac79e51
drmeister <chris.schaf@verizon.net>
2017-11-07 17:01:42 -0500

    Added libunwind-dynamic registration code
    For Linux

--- src/core/debugger.cc
+++ src/core/debugger.cc
@@ -27,6 +27,9 @@ THE SOFTWARE.
 //#define DEBUG_LEVEL_FULL
 
 #include <csignal>
+#ifdef USE_LIBUNWIND
+#include <libunwind.h>
+#endif
 #include <execinfo.h>
 #include <clasp/core/foundation.h>
 #include <clasp/core/object.h>
@@ -46,9 +49,6 @@ THE SOFTWARE.
 #include <clasp/core/lispStream.h>
 #include <clasp/llvmo/llvmoExpose.h>
 #include <clasp/core/wrappers.h>
-#ifdef _TARGET_OS_LINUX
-#include <libunwind.h>
-#endif
 
 namespace core {
 
@@ -454,9 +454,14 @@ CL_DEFUN T_sp core__maybe_demangle(core::String_sp s)
   }
 }
 
-#ifdef _TARGET_OS_DARWIN
 CL_DEFUN T_sp core__libunwind_backtrace_as_list() {
+#ifdef USE_LIBUNWIND
+#ifdef _TARGET_OS_DARWIN
+  ucontext_t context;
+#endif
+#ifdef _TARGET_OS_LINUX
   unw_context_t context;
+#endif
   unw_getcontext(&context);
   unw_cursor_t cursor;
   unw_init_local(&cursor,&context);
@@ -483,9 +488,11 @@ CL_DEFUN T_sp core__libunwind_backtrace_as_list() {
     }
   } while (step>0);
   printf("%s:%d  End of backtrace\n", __FILE__, __LINE__);
+#endif
   return _Nil<T_O>();
 }
-#endif
+
+
 CL_LAMBDA(&optional (depth 0));
 CL_DECLARE();
 CL_DOCSTRING("backtrace");
