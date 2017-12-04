d84ca433f506e06423edf4d6df1de3dc18655bfc
drmeister <chris.schaf@verizon.net>
2017-12-04 11:52:32 -0500

    working on docker stuff

---
+++ tools/prebuild/jupyter/build.lisp
@@ -0,0 +1,62 @@
+(require :asdf)
+
+(progn
+  (load "source-dir:src;lisp;local-asdf-config.lisp")
+  (add-all-subdirs-to-asdf-*central-registry* #P"/Users/meister/quicklisp/"))
+
+
+(time
+ (progn
+   (asdf:load-asd "/Users/meister/Development/cando/tools/prebuild/jupyter/cando-jupyter.asd")
+   (format t "Done asdf:load-asd cando-jupyter.asd~%")))
+
+(time
+ (progn
+  (asdf:make :cando-jupyter)
+  (format t "Done asdf:make~%")))
+
+
+(progn
+  (asdf:operate 'asdf:monolithic-compile-bundle-op :cando-jupyter)
+  (format t "Done asdf:monolithic-compile-bundle-op~%"))
+
+(time
+ (progn
+  (format t "Starting asdf:monolithic-load-bundle-op~%")
+  (asdf:operate 'asdf:monolithic-load-bundle-op :cando-jupyter)
+  (format t "Done asdf:monolithic-load-bundle-op~%")))
+
+(progn
+  (asdf:load-system :cando-jupyter :print t)
+  (format t "Done asdf:load-system :cando-jupyter~%"))
+
+
+(asdf:operate 'asdf:monolithic-load-bundle-op :cando-jupyter)
+
+
+(apropos "monolithic")
+
+
+
+
+
+(load "sys:kernel;cleavir;asdf-system-groveler.lisp")
+
+(time (defparameter *files* (asdf-system-groveler::determine-complete-set-of-asdf-source-files-absolute-path (list :cando :nglview :cl-jupyter))))
+
+(defparameter *cache* #P"/tmp/build/")
+
+(compile-file-build-pathname #P"/Users/meister/quicklisp/dists/quicklisp/software/cl-base64-20150923-git/decode.lisp")
+(compile-all-files *files*)
+
+
+
+*files*
+
+
+
+(asdf:load-system :alexandria)
+
+(asdf/plan:plan-actions 'asdf:concatenate-source-op :alexandria)
+
+(asdf:operate 'asdf:concatenate-source-op :cl-jupyter)
