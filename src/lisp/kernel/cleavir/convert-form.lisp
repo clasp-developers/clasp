43cab8a0882553db45a7ea77c7a9cbcaa46b1492
drmeister <chris.schaf@verizon.net>
2018-01-31 21:59:20 -0500

    cst-to-ast support added

--- src/lisp/kernel/cleavir/convert-form.lisp
+++ src/lisp/kernel/cleavir/convert-form.lisp
@@ -4,13 +4,27 @@
 (defmethod cleavir-generate-ast:convert-code (lambda-list body env (system clasp-cleavir:clasp) &optional block-name )
   (let ((function-ast (call-next-method)))
     (multiple-value-bind (declarations documentation forms)
-	(cleavir-code-utilities:separate-function-body body)
+        (cleavir-code-utilities:separate-function-body body)
       (let* ((dspecs (reduce #'append (mapcar #'cdr declarations)))
-	     (lambda-name (cadr (find 'core:lambda-name dspecs :key #'car))))
-	(unless lambda-name (setq lambda-name 'cl:lambda))
+             (lambda-name (cadr (find 'core:lambda-name dspecs :key #'car))))
+        (unless lambda-name (setq lambda-name 'cl:lambda))
     ;; Make the change here to a named-function-ast with lambda-name
-	(change-class function-ast 'clasp-cleavir-ast:named-function-ast
-		      :lambda-name lambda-name)))))
+        (change-class function-ast 'clasp-cleavir-ast:named-function-ast
+                      :lambda-name lambda-name)))))
+
+
+(defmethod cleavir-cst-to-ast:convert-code (lambda-list body env (system clasp-cleavir:clasp) &optional block-name )
+  (let* ((cst:*ordinary-lambda-list-grammar* clasp-cleavir:*clasp-ordinary-lambda-list-grammar*)
+         (function-ast (call-next-method)))
+    (multiple-value-bind (declaration-csts documentation form-csts)
+        (cst:separate-function-body body)
+      (let* ((dspecs (loop for declaration-cst in declaration-csts
+                           append (cdr (cst:listify declaration-cst))))
+             (lambda-name (cadr (find 'core:lambda-name dspecs :key #'concrete-syntax-tree:first))))
+        (unless lambda-name (setq lambda-name 'cl:lambda))
+    ;; Make the change here to a named-function-ast with lambda-name
+        (change-class function-ast 'clasp-cleavir-ast:named-function-ast
+                      :lambda-name lambda-name)))))
 
 
  
