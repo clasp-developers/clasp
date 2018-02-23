42d1bcd947ef7d07f08090f294f0d9a11887d240
Attila Lendvai <attila@lendvai.name>
2018-02-23 12:30:05 +0100

    scraper: delete obsolete code (they gave compile time warnings)

--- src/scraper/scraper.lisp
+++ src/scraper/scraper.lisp
@@ -32,25 +32,6 @@
 (load "csubst.lisp")
 (load "code-generator.lisp")
 
-(defun update-all-sif-files (all-cc &key regenerate-sifs)
-  "* Arguments
-- all-cc :: A list of all compile-commands.
-- regenerate-sifs :: Force regeneration of all sif files.
-* Description
-Update all of the scraped info files that need updating."
-  (loop for cc in all-cc
-     if (or regenerate-sifs (sif-file-needs-updating cc))
-     do (update-sif-file cc)))
-
-(defun read-all-tags-all-sif-files (all-cc)
-  "* Arguments
-- all-cc :: A list of all of the compile-commands.
-* Description
-Read all of the scraped info files and interpret their tags."
-  (loop for cc in all-cc
-     for tags = (read-sif-file (sif-name cc))
-     nconc tags))
-
 (defparameter *classes* nil)
 (defparameter *gc-managed-types* nil)
 (defparameter *symbols* nil)
@@ -84,33 +65,6 @@ Read all of the scraped info files and interpret their tags."
         (generate-code packages-to-create functions symbols classes gc-managed-types enums initializers build-path app-config))
       (format t "Done scraping code~%"))))
 (export 'process-all-sif-files)
-
-(defun do-scraping (args &key (run-preprocessor t) regenerate-sifs)
-  (declare (optimize (speed 3)))
-  (format t "do-scraping args -> ~a~%" args)
-  (let* ((*clang-path* (first args))
-         (main-path (second args))
-         (*default-pathname-defaults* (make-pathname :name nil :type nil :defaults main-path))
-         (all-commands-fn (third args))
-         (cpp-commands-fn (fourth args))
-         (app-config (setup-application-config *application-config*)))
-    (format t "do-scraping *default-pathname-defaults* -> ~a~%" *default-pathname-defaults*)
-    ;; Run C-preprocessor to generate the missing files
-    (let ((all-cc (read-compile-commands all-commands-fn))
-          (cpp-cc (read-compile-commands cpp-commands-fn)))
-      (when run-preprocessor
-        (format t "About to update all ~d missing preprocessor files~%" (length cpp-cc))
-        (update-cpps cpp-cc)
-        (dolist (cc all-cc)
-          (let ((ifile (merge-pathnames (cpp-name cc) *default-pathname-defaults*)))
-            (unless (probe-file ifile)
-              (format t "Could not find ~a - regenerating it~%" ifile)
-              (run-cpp cc 0)))))
-      ;; update of sif files is done in forked processes
-      #+(or)(update-all-sif-files all-cc :regenerate-sifs regenerate-sifs)
-      (format t "Reading sif files~%")
-      (process-all-sif-files main-path (mapcar (lambda (cc) (sif-name cc)) all-cc)))))
-
 
 (defun report-clang-errors (file command error-string)
   (format *error-output* "Error >>>>>>>> ~a~%" error-string)
@@ -180,15 +134,5 @@ Read all of the scraped info files and interpret their tags."
         (assert (every 'directory-pathname-p (list clasp-home-path build-path)))
         (assert sif-files)
         (process-all-sif-files clasp-home-path build-path sif-files)))))
-    
+
 (export '(generate-one-sif generate-headers-from-all-sifs))
-
-(defun legacy-scraper ()
-  (let ((args (cdr (member "--" sb-ext:*posix-argv* :test #'string=))))
-    (format t "args: ~a~%" args)
-    (format t "*default-pathname-defaults* --> ~a~%" *default-pathname-defaults*)
-    (do-scraping args))
-  (sb-ext:exit))
-
-(export '(legacy-scraper))
-
