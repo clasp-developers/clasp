6c02bcb9fa50abb631f35408083c7ce7b157e9ac
Bike <aeshtaer@gmail.com>
2018-03-27 11:36:00 -0400

    Add test for #417

--- src/lisp/regression-tests/package.lisp
+++ src/lisp/regression-tests/package.lisp
@@ -6,3 +6,9 @@
         (unexport (list (find-symbol "FOO" package) (find-symbol "BAR" package))
                   package-name))
   (delete-package package))
+
+;; Test for #417: characters not working as package designators
+(let ((package (make-package "Z")))
+  (test package-designator-character
+        (gentemp "Y" #\Z)) ; signaled an error before fix
+  (delete-package package))
