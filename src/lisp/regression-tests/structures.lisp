19c673bea417d1cab6290e31f02f90657666b2e9
Karsten Poeck <karsten.poeck@gmail.com>
2018-04-01 13:29:32 +0200

    Add regression tests for recent Prs

--- src/lisp/regression-tests/structures.lisp
+++ src/lisp/regression-tests/structures.lisp
@@ -18,3 +18,58 @@
         (not (eq (copy-structure.foo-bar foo1) (copy-structure.foo-bar foo2)))))
 
 (setf (find-class 'copy-structure.foo) nil)
+
+;; Normal-case
+(test structure-no-keyword-name-normal
+      (let ((instance nil))
+        (defstruct otto a b c)
+        (setq instance (make-otto :a 1 :b 2 :c 3))
+        (multiple-value-bind
+              (copy a b c)
+            (values (copy-otto instance) (otto-a instance)(otto-b instance)(otto-c instance))
+          (and (otto-p copy)(eql a 1)(eql b 2)(eql c 3)))))
+
+;; Following the pattern of above
+(setf (find-class 'otto) nil)
+
+#|
+Bug 475
+Unfortunately this fails without the fix for defstruct, perhaps because of the eval-whens
+
+(test structure-keyword-name-normal
+      (let ((instance nil))
+        (defstruct :otto1 a b c)
+        (setq instance (make-otto1 :a 1 :b 2 :c 3))
+        (multiple-value-bind
+              (copy a b c)
+            (values (copy-otto1 instance) (otto1-a instance)(otto1-b instance)(otto1-c instance))
+          (and (otto1-p copy)(eql a 1)(eql b 2)(eql c 3)))))
+
+(setf (find-class 'otto1) nil)
+
+
+;; has no OTTO2-P by definition in ansi
+(test structure-keyword-name-list
+      (let ((instance nil))
+        (defstruct (:otto2 (:type list)) a b c)
+        (setq instance (make-otto2 :a 1 :b 2 :c 3))
+        (multiple-value-bind
+              (copy a b c)
+            (values (copy-otto2 instance) (otto2-a instance)(otto2-b instance)(otto2-c instance))
+          (and (eql a 1)(eql b 2)(eql c 3)))))
+
+(setf (find-class 'otto2) nil)
+
+;; has no OTTO3-P by definition in ansi
+(test structure-keyword-name-vector
+      (let ((instance nil))
+        (defstruct (:otto3 (:type vector)) a b c)
+        (setq instance (make-otto3 :a 1 :b 2 :c 3))
+        (multiple-value-bind
+              (copy a b c)
+            (values (copy-otto3 instance) (otto3-a instance)(otto3-b instance)(otto3-c instance))
+          (and (eql a 1)(eql b 2)(eql c 3)))))
+
+(setf (find-class 'otto3) nil)
+
+|#
