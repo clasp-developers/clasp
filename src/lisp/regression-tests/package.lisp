6fcad68a89c2455843beff033ab7fb8944dc7fc8
Karsten Poeck <karsten.poeck@gmail.com>
2018-05-19 14:46:35 +0200

    Fix unintern for symbols from core packages, same for delete package, drop nickames when deleting packages

--- src/lisp/regression-tests/package.lisp
+++ src/lisp/regression-tests/package.lisp
@@ -12,3 +12,30 @@
   (test package-designator-character
         (gentemp "Y" #\Z)) ; signaled an error before fix
   (delete-package package))
+
+;; don't allow to unintern t, nil or any symbol of :cl, :keyword and :core
+(test unintern-1
+      (null (unintern t)))
+
+(test unintern-2
+      (null (unintern nil)))
+
+(test unintern-3
+      (null (unintern 'defun)))
+
+(test unintern-4
+      (null (unintern 'every (find-package :cl))))
+
+(test unintern-5
+      (null (unintern :test)))
+
+(test unintern-6
+      (null (unintern 'core:simple-program-error)))
+
+(let ((pkg (make-package "KARSTEN" :nicknames (list "CARLES" "CARLITO"))))
+  (delete-package "KARSTEN")
+  (test nicknames-1 (null (package-nicknames pkg))))
+
+(test nicknames-2 (packagep (make-package "KARSTEN-NEW" :nicknames (list "CARLES" "CARLITO"))))
+ 
+
