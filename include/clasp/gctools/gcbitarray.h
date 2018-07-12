d3f35c40a1baeaffe0f885d3437927ea595df60c
Bike <aeshtaer@gmail.com>
2018-07-12 17:59:14 -0400

    Delete unused GCBitArray_moveable

--- include/clasp/gctools/gcbitarray.h
+++ include/clasp/gctools/gcbitarray.h
@@ -28,36 +28,6 @@ THE SOFTWARE.
 #define gc_gcbitarray_H
 
 namespace gctools {
-
-  template <class T>
-    class GCBitArray_moveable : public GCContainer {
-  public:
-    template <class U, typename Allocator>
-      friend class GCBitArray;
-    typedef T word_type;
-    static const size_t BitWidth = sizeof(word_type)/CHAR_BIT;
-    size_t    _Length; // Index one beyond the total number of elements allocated
-    word_type _Data[0];      // Store _Length numbers of bits with multiple bits per T
-  public:
-  GCBitArray_moveable(size_t length,uint initialValue) : _Length(length) {
-      word_type initialFillValue = (initialValue!=0) ? ~0 : 0;
-      size_t numWords = (length+(BitWidth-1))/BitWidth;
-      for ( size_t i(0); i<this->_Length; ++i ) {
-        this->_Data[i] = initialFillValue;
-      }
-    }
-  GCBitArray_moveable() : _Length(0) {};
-    word_type &operator[](size_t i) { return this->_Data[i]; };
-    const word_type &operator[](size_t i) const { return this->_Data[i]; };
-  };
-
-
-
-} // namespace gctools
-
-
-
-namespace gctools {
 #if BIT_ARRAY_BYTE_SIZE==8
   template <int BitUnitBitWidth, class UnsignedWordType=byte8_t /*uintptr_clasp_t*/, class SignedWordType=int8_t/*intptr_t*/>
 #elif BIT_ARRAY_BYTE_SIZE==32
