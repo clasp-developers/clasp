481568e072a7636bdb7c0847e95cae9d4ebfa508
drmeister <chris.schaf@verizon.net>
2018-07-24 03:11:05 -0400

    Updated workspace-load.lisp

--- src/lisp/kernel/cleavir/workbench-load.lisp
+++ src/lisp/kernel/cleavir/workbench-load.lisp
@@ -1,7 +1,14 @@
 
-(progn
+(defun start-cleavir ()
   (load "sys:kernel;clasp-builder.lsp")
   (defparameter *system*
     (with-open-file (fin "source-dir:tools-for-build;cleavir-file-list.lisp" :direction :input)
       (read fin)))
   (core::load-system *system*))
+
+(defun compile-stuff ()
+  (dotimes (i 50)
+    (format t "Compilation #~a~%" i)
+    (compile-file "sys:kernel;lsp;setf.lsp" :output-file "/tmp/setf.fasl")))
+
+
