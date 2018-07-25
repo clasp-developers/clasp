23de414bb975aa901a3e045d6cb2abb63ee043cf
Bike <aeshtaer@gmail.com>
2018-07-25 11:32:29 -0400

    *print-circle* affects uninterned symbols now
    Should fix #68 and #350

--- src/core/write_object.cc
+++ src/core/write_object.cc
@@ -123,15 +123,16 @@ T_sp write_object(T_sp x, T_sp stream) {
   }
 #endif /* ECL_CMU_FORMAT */
   bool circle = clasp_print_circle();
+  // Most of this is *print-circle* handling- write_ugly_object does the real workd.
+  // We only worry about *print-circle* for objects that aren't numbers, valists, characters,
+  // or interned symbols.
   if (circle &&
       (x) &&
       !x.fixnump() &&
       !x.valistp() &&
       !x.characterp() &&
-      !cl__symbolp(x) &&
-      !cl__numberp(x) // && !x.single_floatp()
-      && (cl__listp(x)
-          || !cl__symbolp(x) || !gc::As<Symbol_sp>(x)->homePackage().nilp())) {
+      !cl__numberp(x) &&
+      (!cl__symbolp(x) || gc::As<Symbol_sp>(x)->homePackage().nilp())) {
     Fixnum code;
     T_sp circle_counter = _sym_STARcircle_counterSTAR->symbolValue();
     if (circle_counter.nilp()) {
