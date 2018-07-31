eea2ad4ed421945ec0cb6de84013f8fae06fa0d6
Karsten Poeck <karsten.poeck@gmail.com>
2018-07-31 18:59:55 +0200

    Move reader-error to package core

--- src/core/exceptions.cc
+++ src/core/exceptions.cc
@@ -746,10 +746,10 @@ CL_DEFUN void core__wrong_index(const string &sourceFile, int lineno, Symbol_sp 
   }
 };
 
-CL_LAMBDA(sourceFileName lineno functionName fmt fmtargs stream);
+CL_LAMBDA(sourceFileName lineno fmt fmtargs stream);
 CL_DECLARE();
 CL_DOCSTRING("readerError");
-CL_DEFUN void cl__reader_error(const string &sourceFile, uint lineno,
+CL_DEFUN void core__reader_error(const string &sourceFile, uint lineno,
                     String_sp fmt, List_sp fmtargs, T_sp stream) {
   ASSERT(cl__stringp(fmt));
   if (stream.nilp()) {
