05d2a044654ff068abd3402a847e96604dcd67a7
Chris Schafmeister <meister@temple.edu>
2018-09-07 10:53:25 -0400

    Add code towards registering JIT DWARF with gdb

--- src/llvmo/llvmoExpose.cc
+++ src/llvmo/llvmoExpose.cc
@@ -3317,6 +3317,49 @@ void dumpObjectFile(size_t num, const char* start, size_t size) {
 }
 #endif
 
+
+////////////////////////////////////////////////////////////
+//
+// Register Jitted object files with gdb
+//
+// Using interface described here:
+//     https://sourceware.org/gdb/current/onlinedocs/gdb/JIT-Interface.html#JIT-Interface
+
+typedef enum
+{
+  JIT_NOACTION = 0,
+  JIT_REGISTER_FN,
+  JIT_UNREGISTER_FN
+} jit_actions_t;
+
+struct jit_code_entry
+{
+  struct jit_code_entry *next_entry;
+  struct jit_code_entry *prev_entry;
+  const char *symfile_addr;
+  uint64_t symfile_size;
+};
+
+struct jit_descriptor
+{
+  uint32_t version;
+  /* This type should be jit_actions_t, but we use uint32_t
+     to be explicit about the bitwidth.  */
+  uint32_t action_flag;
+  struct jit_code_entry *relevant_entry;
+  struct jit_code_entry *first_entry;
+};
+
+/* GDB puts a breakpoint in this function.  */
+void __attribute__((noinline)) __jit_debug_register_code () { }
+
+/* Make sure to specify the version statically, because the
+   debugger may check the version before we can set it.  */
+struct jit_descriptor __jit_debug_descriptor = { 1, 0, 0, 0 };
+
+
+
+
 class ClaspSectionMemoryManager : public SectionMemoryManager {
 
   void 	notifyObjectLoaded (RuntimeDyld &RTDyld, const object::ObjectFile &Obj) {
