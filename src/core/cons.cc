2f4c327a52737a30ad33a81ade5b88abd528fa3c
Karsten Poeck <karsten.poeck@gmail.com>
2018-09-10 20:17:58 +0200

    Fix cl__length for non-proper-list (by drmeister)

--- src/core/cons.cc
+++ src/core/cons.cc
@@ -652,7 +652,11 @@ List_sp Cons_O::copyTreeCar() const {
 
 size_t Cons_O::length() const {
   size_t sz = 1;
-  for (T_sp cur = this->_Cdr; cur.consp(); cur = gc::As_unsafe<Cons_sp>(cur)->_Cdr) ++sz; 
+  T_sp cur = _Nil<T_O>();
+  for (cur = this->_Cdr; cur.consp(); cur = gc::As_unsafe<Cons_sp>(cur)->_Cdr) ++sz;
+  if (cur.notnilp()) {
+    TYPE_ERROR_PROPER_LIST(cur->asSmartPtr());
+  }
   return sz;
 };
 
