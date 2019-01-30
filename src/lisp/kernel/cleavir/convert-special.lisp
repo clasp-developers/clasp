95ea548aab2593e5b5429443b4a5e0b4daa4fb83
Bike <aeshtaer@gmail.com>
2019-01-30 10:41:00 -0500

    Delete obsolete convert-special-binding code

--- src/lisp/kernel/cleavir/convert-special.lisp
+++ src/lisp/kernel/cleavir/convert-special.lisp
@@ -114,21 +114,6 @@
                                   collect `#'(lambda () (progn ,x)))))))
 
 
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;
-;;; convert-special-binding
-;;;
-;;; Convert a special binding to a BIND-AST
-;;;
-;;; Use the primop
-#+(or)(defmethod cleavir-generate-ast::convert-special-binding
-    (variable value-ast next-ast global-env (system clasp-cleavir:clasp))
-  (cleavir-ast:make-bind-ast
-   (cleavir-ast:make-load-time-value-ast `',variable t)
-   value-ast
-   next-ast))
-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
 ;;; Converting CORE:DEBUG-MESSAGE
