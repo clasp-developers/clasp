e69db098e82efe8200fff6b9acf53022a647c2b3
Chris Schafmeister <meister@temple.edu>
2019-04-08 10:51:38 -0400

    Stop using EQUAL for non CL things
    Created special llvm-sys:llvmcontext-equal, llvm-sys:function-equal,
    and llvm-sys:type-equal instead

--- src/lisp/kernel/cleavir/ir.lisp
+++ src/lisp/kernel/cleavir/ir.lisp
@@ -276,7 +276,7 @@ Otherwise do a variable shift."
 (defun %gep (type object indices &optional (label "gep"))
   "Check the type against the object type and if they match return the GEP.
 And convert everything to JIT constants."
-  (unless (equal type (llvm-sys:get-type object))
+  (unless (llvm-sys:type-equal type (llvm-sys:get-type object))
     (error "%gep expected object of type ~a but got ~a of type ~a"
            type object (llvm-sys:get-type object)))
   (let ((converted-indices (mapcar (lambda (x) (%i32 x)) indices)))
