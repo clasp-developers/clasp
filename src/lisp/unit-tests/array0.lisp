b22baff7d6b297e4ae66235c083857e82548ea47
Bike <aeshtaer@gmail.com>
2019-04-25 17:31:00 -0400

    Lose obsolete unit-tests directory

--- src/lisp/unit-tests/array0.lisp
+++
@@ -1,6 +0,0 @@
-
-(test (adjustable-array-p (make-array 10 :adjustable t)) t)
-(test (adjust-array "abcde" 3) "abc")
-(test (type-of (make-array 10 :element-type t :fill-pointer 3)) '(vector t 10))
-(test (typep (make-array 10 :fill-pointer 3) 'simple-array) nil)
-(test (typep (make-array 10 :fill-pointer 3) 'simple-vector) nil)
