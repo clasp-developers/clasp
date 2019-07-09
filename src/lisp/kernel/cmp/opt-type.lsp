clasp-developers/clasp

commit 6593c73a3f4f54030f2d5803da18ae05cf1931f9

Special-case typeq array for efficiency

src/lisp/kernel/cmp/opt-type.lsp
@@ -134,11 +134,14 @@
                               `(eq (core::%array-dimension object 0) ',length)
                               'nil))))
         ((*) ; anything, and dimensions are unspecified
-         (generate-test simple-vector-type complex-vector-type
-                        't 't
-                        (generate-test simple-mdarray-type
-                                       mdarray-type
-                                       't 't 'nil)))
+         ;; for general arrays we have a superclass to use
+         (if (eq uaet '*)
+             `(if (cleavir-primop:typeq object array) t nil)
+             (generate-test simple-vector-type complex-vector-type
+                            't 't
+                            (generate-test simple-mdarray-type
+                                           mdarray-type
+                                           't 't 'nil))))
         (otherwise ; an mdarray with possibly specified dimensions
          `(block nil
             ;; We use a block so the dimensions check code is only

commit 1def1bd7bc5f209d33bf4461a0e58da6932a234e

Fix simple-array typep (#813)
broken by 6593c73a3f4f54030f2d5803da18ae05cf1931f9

src/lisp/kernel/cmp/opt-type.lsp
@@ -134,8 +134,8 @@
                               `(eq (core::%array-dimension object 0) ',length)
                               'nil))))
         ((*) ; anything, and dimensions are unspecified
-         ;; for general arrays we have a superclass to use
-         (if (eq uaet '*)
+         ;; for general arrays we have superclasses to use
+         (if (and (eq uaet '*) (eq simplicity 'array))
              `(if (cleavir-primop:typeq object array) t nil)
              (generate-test simple-vector-type complex-vector-type
                             't 't