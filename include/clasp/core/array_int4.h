22d74d1ea31a5723fdb5b4955f83d923ed29313c
Bike <aeshtaer@gmail.com>
2019-10-22 14:16:07 -0400

    Use correct allocator for byte4 arrays
    allocate_container works, apparently - but I assume it earmarks
    more memory than is actually used or needed

--- include/clasp/core/array_int4.h
+++ include/clasp/core/array_int4.h
@@ -30,9 +30,10 @@ namespace core {
                                value_type initialElement = 0, bool initialElementSupplied = false,
                                size_t initialContentsSize=0, const bit_array_word* initialContents = NULL,
                                bool static_vector_p = false) {
+      (void)static_vector_p; // FIXME: allocate_bitunit_container does not support.
       bit_array_word init = initialFillValue(initialElement);
-      return gctools::GC<my_type>::allocate_container(static_vector_p, length, init, initialElementSupplied,
-                                                      initialContentsSize, initialContents);
+      return gctools::GC<my_type>::allocate_bitunit_container(length, init, initialElementSupplied,
+                                                              initialContentsSize, initialContents);
     }
     smart_ptr_type copy(size_t length, value_type initialElement, bool initialElementSupplied) {
       return make(length, initialElement, initialElementSupplied,
