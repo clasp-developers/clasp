6aaf293cb30dbedecdff04dc3b3e796d95b87d37
Chris Schafmeister <meister@temple.edu>
2020-01-15 07:47:18 -0800

    We don't need to explicitly scan Vec0

--- src/core/lisp_gc.cc
+++ src/core/lisp_gc.cc
@@ -30,7 +30,8 @@ mps_res_t mps_scan_LispRoots(mps_ss_t ss, mps_thr_t thr, void *p, size_t s, Lisp
   mps_scan_InvocationHistoryStack(ss, thr, p, s, this->_InvocationHistoryStack);
   mps_scan_MultipleValues(ss, thr, p, s, this->_MultipleValues);
   mps_scan_DynamicBindingStack(ss, thr, p, s, this->_Bindings);
-  STLMAP_SMART_SECOND_FIX(this->_SourceFiles)
+  // this->_SourceFiles is a Vec0
+//  STLMAP_SMART_SECOND_FIX(this->_SourceFiles)
   GCHOLDER_UNORDEREDSET_FIX(this->_TraceFunctions);
   SMART_PTR_FIX(this->_SystemProperties);
   SMART_PTR_FIX(this->_CatchInfo);
