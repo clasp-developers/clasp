587b25f221eea8286f9cfcfe5acad84684f9dd2c
Bike <aeshtaer@gmail.com>
2020-01-28 16:45:54 -0500

    Fix thread local bindings
    Was getting the no bindings marker returned from symbol-value
    sometimes, because boundp would read data out of bounds and return
    true.

--- src/gctools/threadlocal.cc
+++ src/gctools/threadlocal.cc
@@ -106,6 +106,7 @@ void DynamicBindingStack::set_thread_local_value(T_sp value, const Symbol_O* sym
 bool DynamicBindingStack::thread_local_boundp(const Symbol_O* sym) const {
   uint32_t index = sym->_BindingIdx.load(std::memory_order_relaxed);
   if (index == NO_THREAD_LOCAL_BINDINGS) return false;
+  else if (index >= this->_ThreadLocalBindings.size()) return false;
   else if (gctools::tagged_no_thread_local_bindingp(_ThreadLocalBindings[index].raw_()))
     return false;
   else return true;
