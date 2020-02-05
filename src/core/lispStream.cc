772e5f0413a038574d6b7fd72086782b1efd967d
Неточка Незванова <aeshtaer@gmail.com>
2020-02-05 10:49:59 -0500

    Delete debugging code
    can be added back later if we need it

--- src/core/lispStream.cc
+++ src/core/lispStream.cc
@@ -4418,12 +4418,6 @@ stream_dispatch_table(T_sp strm) {
   if (gc::IsA<Instance_sp>(strm)) {
     return clos_stream_ops;
   }
-  #if 0
-  // drmeister says that just for debugging hashtables in MPS
-  if (!strm) {
-    return startup_stream_ops;
-  }
-  #endif
   ERROR_WRONG_TYPE_ONLY_ARG(core::_sym_dispatchTable, strm, cl::_sym_Stream_O);
 }
 
