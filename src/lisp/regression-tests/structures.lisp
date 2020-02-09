0846b8f1204aa837a9b34c0a19b0665654547b25
Karsten Poeck <karsten.poeck@gmail.com>
2020-02-09 14:27:29 +0100

    More tests with :include and inherited slots

--- src/lisp/regression-tests/structures.lisp
+++ src/lisp/regression-tests/structures.lisp
@@ -119,22 +119,6 @@
               C47
               D47)))
 
-(defstruct node tail-p)
-(defstruct (valued-node (:conc-name node-)
-                        (:include node)))
-(defstruct (combination (:include valued-node)))
-;;; must not define node-tail-p in valued-node, but how to test?
-(test include-level-2
-      (find-if #'(lambda(form)
-                   (and (listp form)
-                        (eql (first form) 'cl:defun)
-                        (eql (second form) 'node-tail-p)))
-               (macroexpand '(defstruct (combination-1 (:conc-name node-)(:include valued-node))))))
-
-;;; Did error when a conc-name is used in the chain, that would repeat an accessor
-(test sbcl-cross-compile-1
-      (fboundp 'combination-tail-p))
-
 (defstruct foo-2 (bar 42 :read-only t))
 
 ;;; if read-only is not repeated, assume it is set (don't require it explicitely)
@@ -148,20 +132,85 @@
               (defstruct (ifoo-2 (:include foo-2 (bar 43 :read-only t))))
               t)))
 
+;;; Should error with something like this (from sbcl)
+;;; The slot BAR is :READ-ONLY in superclass, and so must be :READ-ONLY in subclass.
+;;; This does not error, but at least we don't generate (setf bar)
+;;; To be fixed in override-slotd
 (test-expect-error sbcl-cross-compile-4
                    (eval '(progn
                            (defstruct (ifoo-3 (:include foo-2 (bar 43 :read-only nil))))
                            t)))
-      
-(defstruct bar0 a)
-(defstruct (bar1 (:include bar0)) b)
-;;; must not generate accessor bar0-a
-;;; so it is not enough to look at the immediate parent
-(test include-level-3
+
+;;; tests with include
+(defstruct node tail-p)
+;;; must not define node-tail-p in valued-node, since it would override the function from the parent
+(test include-level-2
+      (not (find-if #'(lambda(form)
+                   (and (listp form)
+                        (eql (first form) 'cl:defun)
+                        (eql (second form) 'node-tail-p)))
+                    (macroexpand '(defstruct (valued-node (:conc-name node-) (:include node)))))))
+
+;;; Should find it , if conc-name leads to different accessor name
+(test include-level-2a
       (find-if #'(lambda(form)
                    (and (listp form)
                         (eql (first form) 'cl:defun)
-                        (eql (second form) 'BAR0-A)))
+                        (eql (second form) 'nodea-tail-p)))
+                    (macroexpand '(defstruct (valued-nodea (:conc-name nodea-) (:include node))))))
+
+(defstruct (valued-node (:conc-name node-) (:include node)))
+
+(defstruct (combination (:include valued-node)))
+;;; should generate combination-tailp, but doesn't
+;;; That breaks cross-compiling sbcl
+;;; Might be wrong in fix-old-slotd
+(test include-level-2b
+      (fboundp 'combination-tail-p))
+
+(defstruct bar0 a)
+(defstruct (bar1 (:include bar0)) b)
+
+;;; must not re-generate accessor bar0-a, but must generate bar0-b & bar0-c
+;;; so it is not enough to look at the immediate parent
+;;; ccl check in sd-refname-in-included-struct-p all the chain up
+(test include-level-3
+      (not (find-if #'(lambda(form)
+                   (and (listp form)
+                        (or (eql (first form) 'cl:defun) #+sbcl (eql (first form) 'SB-C:XDEFUN))
+                        (eql (second form) 'bar0-a)))
+                    (macroexpand '(defstruct (bar2 (:include bar1)(:conc-name bar0-)) c)))))
+
+(test include-level-3a
+      (find-if #'(lambda(form)
+                   (and (listp form)
+                        (or (eql (first form) 'cl:defun) #+sbcl (eql (first form) 'SB-C:XDEFUN))
+                        (eql (second form) 'bar0-b)))
                (macroexpand '(defstruct (bar2 (:include bar1)(:conc-name bar0-)) c))))
  
+(defstruct blah a)
+(defstruct (sub-blah (:include blah)) b)
+(defstruct (sub-sub-blah (:include sub-blah (a 23 :read-only t))) c)
 
+(test-expect-error include-level-4
+                   (let ((object (make-sub-sub-blah)))
+                     (setf (sub-sub-blah-a object) 15)
+                     object))
+;;;sub-blah1-a is not generated, but sub-blah1-b
+(defstruct (sub-blah1 (:include blah)(:conc-name blah-)) b)
+(defstruct (sub-sub-blah1 (:include sub-blah1 (a 23 :read-only t))) c)
+
+(test-expect-error include-level-5
+                   (let ((object (make-sub-sub-blah1)))
+                     (setf (sub-sub-blah1-a object) 15)
+                     object))
+
+(test include-level-5a
+      (let ((object (make-sub-sub-blah1 :a 1 :b 2 :c 3)))
+        (values
+         (sub-sub-blah1-a object)
+         (sub-sub-blah1-b object)
+         (sub-sub-blah1-c object)
+         (setf (sub-sub-blah1-b object) 23)
+         (setf (sub-sub-blah1-b object) 42)
+         object)))
