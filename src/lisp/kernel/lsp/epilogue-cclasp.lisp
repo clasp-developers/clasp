2ab66ac26001596b4e5c68d96b485c1cb4a9ab7c
Chris Schafmeister <meister@temple.edu>
2020-03-08 00:41:07 -0500

    Fix a problem running .clasprc

--- src/lisp/kernel/lsp/epilogue-cclasp.lisp
+++ src/lisp/kernel/lsp/epilogue-cclasp.lisp
@@ -1,13 +1,14 @@
 #+debug-lexical-var-reference-depth
 (eval-when (:compile-toplevel)
   (report-lexical-var-reference-depth))
-#+cclasp(eval-when (:load-toplevel)
-          (cl:in-package :cl-user)
-          (core:process-extension-loads)
-          (core:maybe-load-clasprc)
-          (let ((core:*use-interpreter-for-eval* nil))
-            (core:process-command-line-load-eval-sequence)
-            (if (core:is-interactive-lisp)
-                (core:top-level nil nil)
-                (core:exit 0))))
+#+cclasp
+(eval-when (:load-toplevel)
+  (cl:in-package :cl-user)
+  (let ((core:*use-interpreter-for-eval* nil))
+    (core:process-extension-loads)
+    (core:maybe-load-clasprc)
+    (core:process-command-line-load-eval-sequence)
+    (if (core:is-interactive-lisp)
+        (core:top-level nil nil)
+        (core:exit 0))))
 
