16de4bd6a00168a3571534cf5898a374685a3be6
Bike <aeshtaer@gmail.com>
2020-04-24 16:16:49 -0400

    Default some of the function description fields
    Since NIL indicates no source filename, and so on, this should be
    fine. For the same reason, lambda lists are not default - "nothing
    provided" and "null lambda list" need to be distinguished.

--- include/clasp/core/functor.h
+++ include/clasp/core/functor.h
@@ -82,7 +82,7 @@ struct FunctionDescription {
   void setf_docstring(T_sp);
 };
 
-FunctionDescription* makeFunctionDescription(T_sp functionName, T_sp lambda_list=_Unbound<T_O>(), T_sp docstring=_Unbound<T_O>(), T_sp sourcePathname=_Unbound<T_O>(), int lineno=-1, int column=-1, int filePos=-1);
+FunctionDescription* makeFunctionDescription(T_sp functionName, T_sp lambda_list=_Unbound<T_O>(), T_sp docstring=_Nil<T_O>(), T_sp sourcePathname=_Nil<T_O>(), int lineno=-1, int column=-1, int filePos=-1);
 
 void validateFunctionDescription(const char* filename, size_t lineno, Function_sp function);
 
