6cf3a9430275cd22fded8f497d4d79861c4a4e4c
Bike <aeshtaer@gmail.com>
2020-06-23 08:24:42 -0400

    Always insert type declaration

--- src/lisp/kernel/cleavir/convert-special.lisp
+++ src/lisp/kernel/cleavir/convert-special.lisp
@@ -99,45 +99,38 @@
 
 (defmethod cleavir-cst-to-ast:type-wrap
     (ast ctype origin env (system clasp-cleavir:clasp))
-  (if (cleavir-policy:policy-value
-       (cleavir-env:policy (cleavir-env:optimize-info env))
-       'cleavir-kildall-type-inference:insert-type-checks)
-      ;; Insert type check.
-      (cleavir-cst-to-ast:convert
-       (cst:cst-from-expression
-        (if (and (consp ctype) (eql (first ctype) 'values))
-            (let* (;; For now we don't check optional or rest since our operators
-                   ;; don't really work for that
-                   (required (cleavir-ctype:required ctype system))
-                   (vars (loop repeat (length required) collect (gensym "CHECKED"))))
-              `(let (,@vars)
-                 (cleavir-primop:multiple-value-extract (,@vars)
-                      (cleavir-primop:ast ,ast)
-                   ,@(loop for var in vars
-                           for ty in required
-                           unless (cleavir-ctype:top-p ty system)
-                             collect `(unless (typep ,var ',ty)
-                                        (error 'type-error
-                                               :datum ,var :expected-type ',ty))))))
-            (let ((temp (gensym "CHECKED")))
-              (if (cleavir-ctype:top-p ctype system)
-                  `(cleavir-primop:ast ,ast) ; FIXME: kinda dumb.
-                  `(let ((,temp (cleavir-primop:ast ,ast)))
-                     (if (typep ,temp ',ctype)
-                         ,temp
-                         (error 'type-error
-                                :datum ,temp :expected-type ',ctype)))))))
-       env system)
-      ;; Leave it as a declaration.
-      (if (and (consp ctype) (eql (first ctype) 'values))
-          (cleavir-ast:make-the-ast
-           ast
-           (cleavir-ctype:required ctype system)
-           (cleavir-ctype:optional ctype system)
-           (cleavir-ctype:rest ctype system)
-           :origin origin)
-          (cleavir-ast:make-the-ast
-           ast (list ctype) nil nil :origin origin))))
+  ;; We unconditionally insert a declaration,
+  ;; and insert a type check as well on high safety.
+  ;; NOTE that the type check doesn't check &optional and &rest. FIXME?
+  (let ((value-ast
+          (if (cleavir-policy:policy-value
+               (cleavir-env:policy (cleavir-env:optimize-info env))
+               'cleavir-kildall-type-inference:insert-type-checks)
+              ;; Insert type check.
+              (cleavir-cst-to-ast:convert
+               (cst:cst-from-expression
+                (let* (;; For now we don't check optional or rest since our operators
+                       ;; don't really work for that
+                       (required (cleavir-ctype:required ctype system))
+                       (vars (loop repeat (length required) collect (gensym "CHECKED"))))
+                  `(let (,@vars)
+                     (cleavir-primop:multiple-value-extract (,@vars)
+                         (cleavir-primop:ast ,ast)
+                         ,@(loop for var in vars
+                                 for ty in required
+                                 unless (cleavir-ctype:top-p ty system)
+                                   collect `(unless (typep ,var ',ty)
+                                              (error 'type-error
+                                                     :datum ,var :expected-type ',ty)))))))
+               env system)
+              ;; Leave as declaration.
+              ast)))
+      (cleavir-ast:make-the-ast
+       value-ast
+       (cleavir-ctype:required ctype system)
+       (cleavir-ctype:optional ctype system)
+       (cleavir-ctype:rest ctype system)
+       :origin origin)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
