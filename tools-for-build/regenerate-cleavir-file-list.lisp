f29ec39f4bf9fa8f1049a13c540344685b1d3528
Karsten Poeck <karsten.poeck@gmail.com>
2020-06-28 15:57:02 +0200

    Fix Alexandria, Closer-Mop and Clasp-Cleavir

--- tools-for-build/regenerate-cleavir-file-list.lisp
+++ tools-for-build/regenerate-cleavir-file-list.lisp
@@ -26,6 +26,8 @@
 
 (defun save-file-list-as-python (filename file-list)
   "Save the list of files in FILE-LIST to FILENAME as a python file."
+  (when (probe-file filename)
+    (rename-file filename (make-pathname :type "pyold" :defaults filename) :if-exists :supersede))
   (format t "Saving file list to ~S~%" filename)
   (with-open-file (fout filename :direction :output :if-exists :supersede)
     (format fout "#~%# This is a generated file, editing it is extremely unwise - instead run regenerate-cleavir-file-list.lisp~%#~%")
@@ -37,6 +39,8 @@
 
 (defun save-file-list-as-common-lisp (filename file-list)
   "Save the list of files in FILE-LIST to FILENAME as a common-lisp file."
+  (when (probe-file filename)
+    (rename-file filename (make-pathname :type "lispold" :defaults filename) :if-exists :supersede))
   (format t "Saving file list to ~S~%" filename)
   (with-open-file (fout filename :direction :output :if-exists :supersede)
     (format fout ";;;~%;;; This is a generated file, editing it is extremely unwise - instead run regenerate-cleavir-file-list.lisp~%;;;~%")
@@ -48,7 +52,7 @@
 
 (let* ((here (or *load-truename* *compile-file-truename*))
        (root-clasp (make-pathname :directory
-                        (butlast (pathname-directory here))))
+                                  (butlast (pathname-directory here))))
        (root-sys
          (make-pathname :directory
                         (append (butlast (pathname-directory here)) (list "src" "lisp")))))
@@ -60,11 +64,28 @@
   (setf (logical-pathname-translations "source-dir")
         `((#P"SOURCE-DIR:**;*.*"
              ,(pathname (concatenate 'string (namestring root-clasp) "**/*.*")))))
-  (format t "Sourcedir is  ~a ~%" (translate-logical-pathname "source-dir:"))
-  (load (make-pathname
-         :name "asdf-system-groveler"
-         :type "lisp"
-         :defaults here)))
+  (flet ((fix-system (relative-directory-list system)
+           (let ((directory (make-pathname :directory (append (butlast (pathname-directory here)) relative-directory-list) :defaults nil)))
+             (setf (slot-value system 'asdf/component:relative-pathname) (namestring directory))
+             (setf (slot-value system 'asdf/component:absolute-pathname) (namestring directory))
+             ;;; update the children
+             (dolist (child (asdf/component:component-children system))
+               (setf (slot-value child 'asdf/component:absolute-pathname)
+                     (namestring (make-pathname :directory (pathname-directory directory) :defaults (slot-value child 'asdf/component:absolute-pathname)))))
+             (format t "Sourcedir for ~a is ~a ~%" system (asdf/component:component-pathname system)))))
+    ;;; Alexandria might have the wrong source-pathname
+    (unless (search (namestring root-sys) (namestring (asdf/component:component-pathname (asdf:find-system :alexandria))))
+      (fix-system (list "src" "lisp" "kernel" "contrib" "alexandria")(asdf:find-system :alexandria)))
+    ;;; Closer-mop as well
+    (unless (search (namestring root-sys) (namestring (asdf/component:component-pathname (asdf:find-system :closer-mop))))
+      (fix-system (list "src" "lisp" "kernel" "contrib" "closer-mop")(asdf:find-system :closer-mop)))
+    (unless (search (namestring root-sys) (namestring (asdf/component:component-pathname (asdf:find-system :clasp-cleavir))))
+      (fix-system (list "src" "lisp" "kernel" "cleavir")(asdf:find-system :clasp-cleavir)))
+    (format t "Sourcedir is  ~a ~%" (translate-logical-pathname "source-dir:"))
+    (load (make-pathname
+           :name "asdf-system-groveler"
+           :type "lisp"
+           :defaults here))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
