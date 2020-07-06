dab765859445e79052bfd6dab01f04c692015c6b
Chris Schafmeister <meister@temple.edu>
2020-07-06 17:16:38 -0400

    Change the regression test
    To reflect how we now create 'ast-tooling:compiler-instance

--- src/lisp/regression-tests/stamps.lisp
+++ src/lisp/regression-tests/stamps.lisp
@@ -10,7 +10,7 @@
 ;; Test that wrapped classes get their stamp from the wrapper
 #-use-mps
 (test stamp-from-wrapper
-      (= (core:instance-stamp (core:make-cxx-object 'ast-tooling:compiler-instance))
+      (= (core:instance-stamp (ast-tooling:make-compiler-instance))
          (core:class-stamp-for-instances (find-class 'ast-tooling:compiler-instance))))
 ;;  This last one is to test Derivable classes
 ;;     I set up a special case for these where it calls the General_O::get_stamp_() method - but it doesn't look like it is necessary
