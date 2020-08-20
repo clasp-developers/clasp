3f700bf71c00c3c3e350410acb2d2fb2c95f896c
Bike <aeshtaer@gmail.com>
2020-08-20 10:43:20 -0400

    Conditionalize create definitions

--- include/clasp/core/bignum.h
+++ include/clasp/core/bignum.h
@@ -308,15 +308,18 @@ public: // Functions here
   static TheNextBignum_sp create(gc::Fixnum fix) {
     return create_from_limbs((fix < 0) ? -1 : 1, std::abs(fix), true);
   }
+#if !defined( CLASP_UNSIGNED_LONG_LONG_IS_UINT64 )
   static TheNextBignum_sp create(unsigned long long ull) {
     ASSERT(sizeof(unsigned long long)<=sizeof(mp_limb_t));
     return create_from_limbs(1, ull, true);
   }
+#endif
+#if !defined( CLASP_LONG_LONG_IS_INT64 )
   static TheNextBignum_sp create(long long ll) {
     ASSERT(sizeof(long long)<=sizeof(mp_limb_t));
     return create_from_limbs((ll < 0) ? -1 : 1, std::abs(ll), true);
   }
-
+#endif
 #if !defined(CLASP_FIXNUM_IS_INT64)
   static TheNextBignum_sp create(int64_t v) {
     return create_from_limbs((v < 0) ? -1 : 1, std::abs(v), true);
