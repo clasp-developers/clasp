5098b3b7c9fc04f5881404aa1d9678e60a283bdf
Charles Zhang <charleszhang99@yahoo.com>
2020-11-10 14:06:01 -0800

    Use mkstemp on regression test.

--- src/lisp/regression-tests/unwind.lisp
+++ src/lisp/regression-tests/unwind.lisp
@@ -3,5 +3,8 @@
 ;; Assert that this compilation does not invoke any unwinds.
 (test compile-file-no-unwind
       (let ((unwinds (gctools:thread-local-unwinds)))
-        (cmp::compile-file-serial "sys:kernel;lsp;predlib.lsp")
+        (cmp::compile-file-serial "sys:kernel;lsp;predlib.lsp"
+                                  :output-file (make-pathname
+                                                :type (pathname-type (compile-file-pathname "foo.lisp"))
+                                                :defaults (core:mkstemp "TMP:predlib")))
         (= unwinds (gctools:thread-local-unwinds))))
