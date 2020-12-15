c104fb2669a856cf3ecd165bd34ce308390764b7
Charles Zhang <charleszhang99@yahoo.com>
2020-12-15 12:35:19 -0800

    Make sure not to lose multiple values when type checking.

--- src/lisp/kernel/cleavir/convert-special.lisp
+++ src/lisp/kernel/cleavir/convert-special.lisp
@@ -89,16 +89,16 @@
        ;; We don't want to insert type checks for typep and error, or
        ;; else we risk getting trapped in an infinite recursion.
        (declare (optimize (safety 0)))
-       ,@(loop for var in vars
-               for ty in required
-               unless (cleavir-ctype:top-p ty system)
-                 collect `(if (typep ,var
-                                     ',(discrimination-type
-                                        ty))
-                              ,var
-                              (error 'type-error
-                                     :datum ,var
-                                     :expected-type ',ty))))))
+       (values ,@(loop for var in vars
+                       for ty in required
+                       unless (cleavir-ctype:top-p ty system)
+                         collect `(if (typep ,var
+                                             ',(discrimination-type
+                                                ty))
+                                      ,var
+                                      (error 'type-error
+                                             :datum ,var
+                                             :expected-type ',ty)))))))
 
 (defmethod cleavir-cst-to-ast:type-wrap
     (ast ctype origin env (system clasp-cleavir:clasp))
