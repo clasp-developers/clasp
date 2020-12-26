cb8a2e3c5c00e4443d7d6b89138d388980bb636e
Christian Schafmeister <meister@temple.edu>
2020-12-26 15:47:31 -0500

    Fixed a problem with MPS build

--- include/clasp/gctools/gcalloc.h
+++ include/clasp/gctools/gcalloc.h
@@ -206,28 +206,28 @@ struct ConsSizeCalculator<Cons,DontRegister> {
 #ifdef USE_MPS
   extern void bad_cons_mps_reserve_error();
 
-template <typename Register, typename... ARGS>
+template <typename ConsType, typename Register, typename... ARGS>
 #ifdef ALWAYS_INLINE_MPS_ALLOCATIONS
   __attribute__((always_inline))
 #else
     inline
 #endif
-    smart_ptr<Cons> cons_mps_allocation(mps_ap_t& allocation_point,
+smart_ptr<ConsType> cons_mps_allocation(mps_ap_t& allocation_point,
                                                const char* ap_name,
                                                ARGS &&... args) {
-    gc::smart_ptr<Cons_O> tagged_obj;
+  gc::smart_ptr<ConsType> tagged_obj;
     { RAII_DISABLE_INTERRUPTS();
       RAII_DEBUG_RECURSIVE_ALLOCATIONS((size_t)STAMP_CONS);
       // printf("%s:%d cons_mps_allocation\n", __FILE__, __LINE__ );
       mps_addr_t addr;
-      Cons_O* cons;
-      size_t cons_size = ConsSizeCalculator<Cons,Register>::value();
+      ConsType* cons;
+      size_t cons_size = ConsSizeCalculator<ConsType,Register>::value();
       do {
         mps_res_t res = mps_reserve(&addr, allocation_point, cons_size);
         if ( res != MPS_RES_OK ) bad_cons_mps_reserve_error();
-        cons = reinterpret_cast<Cons_O*>(addr);
-        new (cons) Cons_O(std::forward<ARGS>(args)...);
-        tagged_obj = smart_ptr<Cons_O>((Tagged)tag_cons(cons));
+        cons = reinterpret_cast<ConsType*>(addr);
+        new (cons) ConsType(std::forward<ARGS>(args)...);
+        tagged_obj = smart_ptr<ConsType>((Tagged)tag_cons(cons));
       } while (!mps_commit(allocation_point, addr, cons_size));
       MAYBE_VERIFY_ALIGNMENT((void*)addr);
       //      printf("%s:%d cons_mps_allocation addr=%p size=%lu\n", __FILE__, __LINE__, addr, sizeof(Cons));
@@ -454,7 +454,7 @@ template <class Cons, class Register>
         mps_ap_t obj_ap = my_thread_allocation_points._cons_allocation_point;
         //        globalMpsMetrics.consAllocations++;
         smart_ptr<Cons> obj =
-            cons_mps_allocation<Register>(obj_ap,"CONS",
+            cons_mps_allocation<Cons,Register>(obj_ap,"CONS",
                               std::forward<ARGS>(args)...);
         return obj;
 #endif
