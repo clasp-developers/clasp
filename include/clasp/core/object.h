clasp-developers/clasp

commit 2348844606c8c408b802cda155b9b32f1158a19d

equalp and vector/array fixes

include/clasp/core/object.h
@@ -823,17 +823,24 @@ inline bool cl_equal(T_sp x, T_sp y) {
   return x->equal(y);
 };
 
+ 
+extern int basic_compare(Number_sp na, Number_sp nb);
+
 #define ARGS_cl_equalp "(x y)"
 #define DECL_cl_equalp ""
 #define DOCS_cl_equalp "equalp"
 inline bool cl_equalp(T_sp x, T_sp y) {
   if (x.fixnump()) {
-    return x.raw_() == y.raw_();
+    if ( y.fixnump() ) {
+      return x.raw_() == y.raw_();
+    } else {
+      return basic_compare(x,y) == 0;
+    }
   } else if (x.single_floatp()) {
     if (y.single_floatp()) {
       return gc::tagged_single_float_masked(x.raw_()) == gc::tagged_single_float_masked(y.raw_());
     }
-    return false;
+    return basic_compare(x,y);
   } else if (x.characterp()) {
     return clasp_charEqual2(x, y);
   }

include/clasp/core/array.h
@@ -58,7 +58,7 @@ public:
   static int checkedIndex(const string &filename, int lineno, const string &function, Array_sp array, int which, T_sp index, int nonincl_index);
 
 public: // Functions here
-  virtual bool equalp(T_sp other) const;
+  virtual bool equalp(T_sp other) const {SUBIMP();};
   virtual T_sp aset_unsafe(int j, T_sp val) { SUBIMP(); };
   virtual bool arrayHasFillPointerP() const { return false; };
   virtual gc::Fixnum arrayTotalSize() const;

include/clasp/core/arrayObjects.h
@@ -59,6 +59,7 @@ public: // Functions here
   static ArrayObjects_sp make(T_sp dim, T_sp elementType, T_sp initialElement, T_sp adjustable);
 
 public:
+  virtual bool equalp(T_sp other) const;
   virtual T_sp aset_unsafe(int j, T_sp val);
   T_sp elementType() const { return this->_ElementType; };
 
@@ -71,7 +72,6 @@ public:
   virtual std::vector<cl_index> dimensions() const { return this->_Dimensions; };
   virtual gc::Fixnum arrayDimension(gc::Fixnum axisNumber) const;
   virtual T_sp aref_unsafe(cl_index index) const { return this->_Values[index];};
-
 
   LongLongInt setDimensions(List_sp dims, T_sp initialElement);
 

include/clasp/core/foundation.h
@@ -669,6 +669,9 @@ typedef gctools::smart_ptr</* TODO: use const */ T_O> T_sp;
 class Class_O;
 typedef gctools::smart_ptr<Class_O> Class_sp;
 
+class Number_O;
+typedef gctools::smart_ptr<Number_O> Number_sp;
+ 
 class VectorObjects_O;
 typedef gctools::smart_ptr<VectorObjects_O> VectorObjects_sp;
 

include/clasp/core/lispVector.h
@@ -54,6 +54,7 @@ public:
 
 private: // instance variables here
 public:  // Functions here
+  bool equalp(T_sp o) const;
   bool adjustableArrayP() const { return false; };
   gc::Fixnum vector_length() const { return this->dimension(); };
   virtual gc::Fixnum dimension() const { SUBIMP(); };

src/core/array.cc
@@ -267,10 +267,6 @@ struct RecursivePrint {
   }
 };
 
-bool Array_O::equalp(T_sp other) const {
-  IMPLEMENT_MEF(BF("Implement Array_O::equalp"));
-}
-
 string Array_O::__repr__() const {
   _G();
   RecursivePrint rp(this->asSmartPtr());

src/core/arrayObjects.cc
@@ -107,6 +107,33 @@ T_sp ArrayObjects_O::aset_unsafe(int idx, T_sp value) {
   return value;
 }
 
+bool ArrayObjects_O::equalp(T_sp o) const {
+  if ( this->eq(o) ) return true;
+  if ( ArrayObjects_sp other = o.asOrNull<ArrayObjects_O>() ) {
+    const std::vector<cl_index>& my_dimensions = this->_Dimensions;
+    const std::vector<cl_index>& other_dimensions = other->_Dimensions;
+    if ( my_dimensions.size() != other_dimensions.size() ) return false;
+    size_t size = 1;
+    for ( int i(0); i<my_dimensions.size(); ++i ) {
+      size *= my_dimensions[i];
+      if ( my_dimensions[i] != other_dimensions[i] ) return false;
+    }
+    for ( size_t i(0); i<size; ++i ) {
+      if ( !cl_equalp(this->rowMajorAref(i),other->rowMajorAref(i)) ) return false;
+    }
+    return true;
+  } if ( Vector_sp vec = o.asOrNull<Vector_O>() ) {
+    if ( this->_Dimensions.size() != 1 ) return false;
+    if ( this->_Dimensions[0] != cl_length(vec) ) return false;
+    size_t size = this->_Dimensions[0];
+    for ( int i(0); i<size; ++i ) {
+      if ( !cl_equalp(this->rowMajorAref(i),vec->aref_unsafe(i)) ) return false;
+    }
+    return true;
+  }
+  return false;
+}
+
 T_sp ArrayObjects_O::rowMajorAref(cl_index idx) const {
   _G();
   ASSERTF(idx < this->_Values.size(), BF("Illegal row-major-aref index %d - must be less than %d") % idx % this->_Values.size());

src/core/lispVector.cc
@@ -144,6 +144,30 @@ List_sp Vector_O::arrayDimensions() const {
   return Cons_O::create(make_fixnum(this->dimension()), _Nil<T_O>());
 }
 
+bool Vector_O::equalp(T_sp o) const {
+  if ( this->eq(o) ) return true;
+  if ( Array_sp other = o.asOrNull<Array_O>() ) {
+    if (other->rank() != 1 ) return false;
+    size_t my_size = this->length();
+    if ( other->arrayDimension(0) != my_size ) return false;
+    for ( size_t i(0); i<my_size; ++i ) {
+      if ( !cl_equalp(this->svref(i),other->rowMajorAref(i)) ) return false;
+    }
+    return true;
+  } if ( Vector_sp vec = o.asOrNull<Vector_O>() ) {
+    size_t my_size = this->length();
+    size_t other_size = vec->length();
+    if ( my_size != other_size ) return false;
+    for ( int i(0); i<my_size; ++i ) {
+      if ( !cl_equalp(this->aref_unsafe(i),vec->aref_unsafe(i)) ) return false;
+    }
+    return true;
+  }
+  return false;
+}
+
+
+
 T_sp Vector_O::aref(List_sp args) const
 {
   cl_index idx = this->index(args);

commit f276e6867a807845bea17e893ffc3723e4c5ac13

Fix equalp on single-float with different type
Previously (equalp single bignum) and (equalp single double)
always returned true, as far as I can tell.

I think this bug was introduced in
2348844606c8c408b802cda155b9b32f1158a19d way back in 2015, but if
so, it's kind of crazy that we haven't noticed before. I guess
equalp isn't actually used that often, and less often on numbers.

Fixes #1110 which was indirectly caused by this.

include/clasp/core/object.h
@@ -617,7 +617,7 @@ namespace core {
     SIMPLE_ERROR_SPRINTF("Bad equal comparison");
   };
 
-  extern int basic_compare(Number_sp na, Number_sp nb);
+  extern bool basic_equalp(Number_sp na, Number_sp nb);
 
   bool cl__equalp(T_sp x, T_sp y);
 };

src/core/object.cc
@@ -648,19 +648,19 @@ CL_DEFUN bool cl__equalp(T_sp x, T_sp y) {
         return x.raw_() == y.raw_();
       } else if (y.single_floatp()) {
         return (x.unsafe_fixnum() == y.unsafe_single_float());
-      } else if (Number_sp ny = y.asOrNull<Number_O>()) {
-        return basic_compare(gc::As_unsafe<Fixnum_sp>(x), ny) == 0;
-      }
-      return false;
+      } else if (gc::IsA<Number_sp>(y)) {
+        return basic_equalp(gc::As_unsafe<Fixnum_sp>(x),
+                            gc::As_unsafe<Number_sp>(y));
+      } else return false;
     } else if (x.single_floatp()) {
       if (y.single_floatp()) {
         return x.unsafe_single_float() == y.unsafe_single_float();
       } else if (y.fixnump()) {
         return x.unsafe_single_float() == y.unsafe_fixnum();
-      } else if (Number_sp ny = y.asOrNull<Number_O>()) {
-        return basic_compare(gc::As<SingleFloat_sp>(x), ny);
-      }
-      return false;
+      } else if (gc::IsA<Number_sp>(y)) {
+        return basic_equalp(gc::As_unsafe<SingleFloat_sp>(x),
+                            gc::As_unsafe<Number_sp>(y));
+      } else return false;
     } else if (x.characterp()) {
       return clasp_charEqual2(x, y);
     } else if (x.consp() ) {