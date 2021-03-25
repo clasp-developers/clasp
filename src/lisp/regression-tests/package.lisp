482c4143f7a23df3d29cfc5037eba50819bcd1ed
Bike <aeshtaer@gmail.com>
2021-03-25 16:43:04 -0400

    Define tests for name conflict resolution

--- src/lisp/regression-tests/package.lisp
+++ src/lisp/regression-tests/package.lisp
@@ -25,8 +25,7 @@
 (test unintern-3
       (null (unintern 'defun)))
 
-(test unintern-4
-      (null (unintern 'every (find-package :cl))))
+(test-expect-error unintern-4 (unintern 'every "CL"))
 
 (test unintern-5
       (null (unintern :test)))
@@ -52,7 +51,7 @@
   (test-expect-error
    nicknames-3
    (let () (make-package "KARSTEN-OLD" :nicknames (list "pepito")))
-   :type core:simple-package-error)
+   :type package-error)
   (when (find-package "KARSTEN-NEW")
     (delete-package (find-package "KARSTEN-NEW")))
   (when (find-package "KARSTEN-OLD")
@@ -64,7 +63,7 @@
    packages-1
    (let ()
      (make-package "KARSTEN-NEW"))
-   :type core:simple-package-error)
+   :type package-error)
   (when (find-package "KARSTEN-NEW")
     (delete-package (find-package "KARSTEN-NEW"))))
 
@@ -147,3 +146,169 @@
            (ext:package-remove-nickname package-desig :inexistant-nickname)))
    :type package-error)
 
+;;; Name conflict and resolution tests
+
+(let* ((nc0 (make-package "NC0")) (nc1 (make-package "NC1"))
+       (sname "TEST") (rname "TSET")
+       (s0 (intern sname nc0)) (s1 (intern sname nc1))
+       (r0 (intern rname nc0)) (r1 (intern rname nc1)))
+  (test import-conflict-0
+        (handler-case
+            (progn (import s1 nc0) nil)
+          (ext:name-conflict (c)
+            (let ((candidates (ext:name-conflict-candidates c)))
+              (or (equal candidates (list s0 s1))
+                  (equal candidates (list s1 s0)))))))
+  ;; Test that RESOLVE-CONFLICT only allows the conflicting symbols
+  ;; (and also, transfers control)
+  (test-expect-error import-conflict-1
+                     (block nil
+                       (handler-bind
+                           ((ext:name-conflict
+                              (lambda (c)
+                                (invoke-restart
+                                 (find-restart 'ext:resolve-conflict c) r0)
+                                (return nil))))
+                         (progn (import s1 nc0) nil))))
+  ;; Resolving a conflict in favor of the new symbol
+  (test import-conflict-2
+        (handler-bind
+            ((ext:name-conflict
+               (lambda (c)
+                 (invoke-restart (find-restart 'ext:resolve-conflict c) s1))))
+          (progn (import s1 nc0)
+                 ;; Check that s0 has been uninterned and replaced by s1
+                 (and (null (symbol-package s0))
+                      (equal (multiple-value-list (find-symbol sname nc0))
+                             (list s1 :internal))))))
+  ;; Resolving a conflict in favor of the old symbol
+  (test import-conflict-3
+        (handler-bind
+            ((ext:name-conflict
+               (lambda (c)
+                 (invoke-restart (find-restart 'ext:resolve-conflict c) r0))))
+          (progn (import r1 nc0)
+                 (equal (multiple-value-list (find-symbol rname nc0))
+                        (list r0 :internal)))))
+  (delete-package nc0) (delete-package nc1))
+
+;; Conflicts between a present symbol and a newly inherited symbol
+(let* ((nc0 (make-package "NC0")) (nc1 (make-package "NC1"))
+       (aname "A") (a0 (intern aname nc0)) (a1 (intern aname nc1))
+       (bname "B") (b0 (intern bname nc0)) (b1 (intern bname nc1)))
+  (export (list a1 b1) nc1)
+  ;; Exactly two conflicts are signaled and resolvable
+  (test use-package-conflict-0
+        (let ((a-resolved nil) (b-resolved nil))
+          (block nil
+            (handler-bind
+                ((ext:name-conflict
+                   (lambda (c)
+                     (let ((candidates (ext:name-conflict-candidates c)))
+                       (cond ((member a1 candidates)
+                              (if (and (not a-resolved)
+                                       (or (equal candidates (list a0 a1))
+                                           (equal candidates (list a1 a0))))
+                                  (setf a-resolved t)
+                                  (return nil))
+                              (invoke-restart 'ext:resolve-conflict a0))
+                             ((member b1 candidates)
+                              (if (and (not b-resolved)
+                                       (or (equal candidates (list b0 b1))
+                                           (equal candidates (list b1 b0))))
+                                  (setf b-resolved t)
+                                  (return nil))
+                              (invoke-restart 'ext:resolve-conflict b1))
+                             (t (return nil)))))))
+              (use-package nc1 nc0))
+            (and a-resolved b-resolved))))
+  ;; a0, the old present symbol, was chosen: a0 shadows.
+  (test use-package-conflict-1
+        (and (equal (multiple-value-list (find-symbol aname nc0))
+                    (list a0 :internal))
+             (member a0 (package-shadowing-symbols nc0))))
+  ;; b1, the new symbol, was chosen: b0 is uninterned.
+  (test use-package-conflict-2
+        (and (equal (multiple-value-list (find-symbol bname nc0))
+                    (list b1 :inherited))
+             (null (symbol-package b0))))
+  (delete-package nc0) (delete-package nc1))
+
+;; Conflicts between two inherited symbols
+;; These are separate in part because as far as I can tell, SBCL currently gets
+;; this slightly wrong by interning a new symbol in the child package, instead
+;; of shadowing-import-ing one of the existing ones.
+(let* ((par0 (make-package "PAR0")) (par1 (make-package "PAR1"))
+       (aname "A") (a0 (intern aname par0)) (a1 (intern aname par1))
+       (bname "B") (b0 (intern bname par0)) (b1 (intern bname par1))
+       (chil (make-package "CHIL" :use (list par0))))
+  (export (list a0 b0) par0) (export (list a1 b1) par1)
+  ;; Exactly two conflicts are signaled and resolvable
+  (test use-package-conflict-3
+        (let ((a-resolved nil) (b-resolved nil))
+          (block nil
+            (handler-bind
+                ((ext:name-conflict
+                   (lambda (c)
+                     (let ((candidates (ext:name-conflict-candidates c)))
+                       (cond ((member a1 candidates)
+                              (if (and (not a-resolved)
+                                       (or (equal candidates (list a0 a1))
+                                           (equal candidates (list a1 a0))))
+                                  (setf a-resolved t)
+                                  (return nil))
+                              (invoke-restart 'ext:resolve-conflict a0))
+                             ((member b1 candidates)
+                              (if (and (not b-resolved)
+                                       (or (equal candidates (list b0 b1))
+                                           (equal candidates (list b1 b0))))
+                                  (setf b-resolved t)
+                                  (return nil))
+                              (invoke-restart 'ext:resolve-conflict b1))
+                             (t (return nil)))))))
+              (use-package par1 chil))
+            (and a-resolved b-resolved))))
+  ;; a0 from par0 was chosen by shadowing-import-ing it.
+  (test use-package-conflict-4
+        (and (equal (multiple-value-list (find-symbol aname chil))
+                    (list a0 :internal))
+             (member a0 (package-shadowing-symbols chil))))
+  ;; b1 from par1 was chosen by shadowing-import-ing it.
+  (test use-package-conflict-5
+        (and (equal (multiple-value-list (find-symbol bname chil))
+                    (list b1 :internal))
+             (member b1 (package-shadowing-symbols chil))))
+  (delete-package chil) (delete-package par0) (delete-package par1))
+
+(let* ((par0 (make-package "PAR0")) (par1 (make-package "PAR1"))
+       (par2 (make-package "PAR2")) (chil (make-package "CHIL"))
+       (sname "TEST") (s0 (intern sname par0)) (s1 (intern sname par1))
+       (s2 (intern sname par2)) (sc (intern sname chil)))
+  (shadow (list sname) chil)
+  (export s0 par0) (export s1 par1) (export s2 par2)
+  (use-package (list par0 par1 par2) chil)
+  (test unintern-conflict-0
+        (handler-case (unintern sc chil)
+          (ext:name-conflict (c)
+            (let ((candidates (ext:name-conflict-candidates c)))
+              (and (= (length candidates) 3)
+                   (member s0 candidates :test #'eq)
+                   (member s1 candidates :test #'eq)
+                   (member s2 candidates :test #'eq))))))
+  ;; Conflict can be resolved
+  (test unintern-conflict-1
+        (handler-bind
+            ((ext:name-conflict
+               (lambda (c)
+                 (declare (ignore c))
+                 (invoke-restart 'ext:resolve-conflict s2))))
+          (progn (unintern sc chil) t)))
+  ;; sc is actually uninterned
+  (test unintern-conflict-2 (null (symbol-package sc)))
+  ;; s2 was shadowing-import-ed
+  (test unintern-conflict-3
+        (and (equal (multiple-value-list (find-symbol sname chil))
+                    (list s2 :internal))
+             (member s2 (package-shadowing-symbols chil))))
+  (delete-package chil)
+  (delete-package par0) (delete-package par1) (delete-package par2))
