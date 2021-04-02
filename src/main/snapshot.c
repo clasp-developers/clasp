e6736d394f52e632dfc20ca52f856417440b22fa
Christian Schafmeister <meister@temple.edu>
2021-04-02 13:57:05 -0400

    Generalize incbin

--- src/main/snapshot.c
+++ src/main/snapshot.c
@@ -8,7 +8,7 @@ __asm__("\n\
     .align 8\n\
     .global start_of_snapshot\n\
 start_of_snapshot:\n\
-    .incbin \"/home/meister/Development/clasp-future/build/boehmprecise/generated/cclasp-boehmprecise-snapshot.snapshot\"\n\
+    .incbin \"generated/cclasp-boehmprecise-snapshot.snapshot\"\n\
     .global end_of_snapshot\n\
 end_of_snapshot:\n\
     .popsection\n");
