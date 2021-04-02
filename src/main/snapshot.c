eb429f294940b1cb9893ebcf207b2e20901c52ae
Christian Schafmeister <meister@temple.edu>
2021-04-02 12:44:34 -0400

    Added missing file for snapshot save/load

---
+++ src/main/snapshot.c
@@ -0,0 +1,16 @@
+
+
+
+void include_snapshot() {
+
+__asm__("\n\
+    .pushsection .clasp_snapshot, \"a\", @progbits\n\
+    .align 4\n\
+    .global start_of_snapshot\n\
+start_of_snapshot:\n\
+    .incbin \"/home/meister/Development/clasp-future/build/boehmprecise/generated/cclasp-boehmprecise-snapshot.snapshot\"\n\
+    .global end_of_snapshot\n\
+end_of_snapshot:\n\
+    .popsection\n");
+
+}
