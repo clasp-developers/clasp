650d4bc41a97193a4d0a49b6f0f997985e049139
Bike <aeshtaer@gmail.com>
2021-04-07 14:50:46 -0400

    Simplify MTF translation
    without mv-setq, there's only one output, so mtf is trivial

--- src/lisp/kernel/cleavir/translate.lisp
+++ src/lisp/kernel/cleavir/translate.lisp
@@ -327,14 +327,7 @@
          (rv (first inputs))
          (destination (bir:destination instruction))
          (destination-id (get-destination-id destination)))
-    ;; We can only transmit multiple values, so make sure the adapter in
-    ;; bir.lisp forced that properly
-    (ecase (length inputs)
-      ;; GO
-      (0)
-      ;; RETURN-FROM
-      (1 (assert (bir:rtype= (bir:rtype rv) :multiple-values))))
-    ;; Transmit those values
+    ;; Transmit values
     (when rv
       (save-multiple-value-0 (in rv)))
     ;; unwind
@@ -739,70 +732,9 @@
 
 (defmethod translate-simple-instruction
     ((instr bir:multiple-to-fixed) (abi abi-x86-64))
-  ;; Outputs that are returned in registers (see +pointers-returned-in-registers+) can be
-  ;; unconditionally assigned, as things that return values ensure that those return registers
-  ;; are always valid - e.g., (values) explicitly sets them to NIL.
-  ;; Beyond that, we have to branch on nret.
-  (let* ((mv (in (first (bir:inputs instr))))
-         (outputs (bir:outputs instr))
-         (nouts (length outputs)))
-    ;; The easy one:
-    (unless (zerop nouts)
-      (out (cmp:irc-tmv-primary mv) (first outputs)))
-    ;; Now do the branch stuff (if there are enough outputs to require it)
-    ;; We end up with a switch on nret. Say we have three outputs and +p-r-i-r+ is 1;
-    ;; then we want
-    ;; out[0] = values0; // values0 is a register
-    ;; switch (nret) {
-    ;; case 0: // don't need to bother with out[0] any more, so fallthrough
-    ;; case 1: out[1] = nil; out[2] = nil; break;
-    ;; case 2: out[1] = values[1]; out[2] = nil; break;
-    ;; default: out[1] = values[1]; out[2] = values[1]; break; // any extra values ignored
-    ;; }
-    ;; We generate SSA directly, so the assignments are just phis.
-    (when (> nouts +pointers-returned-in-registers+)
-      (let* ((rets (loop for i from +pointers-returned-in-registers+
-                           below nouts
-                         collect (return-value-elt i)))
-             (default (cmp:irc-basic-block-create "mtf-enough"))
-             (switch (cmp:irc-switch (cmp:irc-tmv-nret mv) default nouts))
-             (final (cmp:irc-basic-block-create "mtf-final"))
-             ;; Generate the default block, while keeping values for the phis.
-             (default-vars (prog2 (cmp:irc-begin-block default)
-                               (mapcar #'cmp:irc-load rets)
-                             (cmp:irc-br final)))
-             ;; Generate the switch blocks. Put them in the switch while we're at it.
-             ;; The binding here is to a list of (block . vars) so we can phi it.
-             (blocks-and-vars
-               (loop for retn from +pointers-returned-in-registers+ below nouts
-                     for block = (cmp:irc-basic-block-create (format nil "mtf-~d" retn))
-                     do (cmp:irc-add-case switch (%size_t retn) block)
-                        (cmp:irc-begin-block block)
-                     collect (cons block
-                                   (loop for ret in rets
-                                         for i from +pointers-returned-in-registers+ below nouts
-                                         collect (if (< i retn) (cmp:irc-load ret) (%nil))))
-                     do (cmp:irc-br final))))
-        ;; Set up all the register-only cases to use the first block.
-        ;; (which sets all the outputs to NIL)
-        (loop with low = (caar blocks-and-vars)
-              for retn from 0 below +pointers-returned-in-registers+
-              do (cmp:irc-add-case switch (%size_t retn) low))
-        ;; Final generation: generate the phis and then output them.
-        ;; NOTE: We can't output as we generate because (out ...) may generate a store,
-        ;; and phis must not have any stores (or anything but a phi) preceding them.
-        (cmp:irc-begin-block final)
-        (let* ((vector-outs (nthcdr +pointers-returned-in-registers+ outputs))
-               (phis (loop for out in vector-outs
-                           for i from 0
-                           for phi = (cmp:irc-phi cmp:%t*% (1+ nouts))
-                           do (loop for (block . vars) in blocks-and-vars
-                                    do (cmp:irc-phi-add-incoming phi (elt vars i) block))
-                              (cmp:irc-phi-add-incoming phi (elt default-vars i) default)
-                           collect phi)))
-          (loop for phi in phis
-                for out in vector-outs
-                do (out phi out)))))))
+  (assert (= (length (bir:outputs instr)) 1))
+  (out (cmp:irc-tmv-primary (in (first (bir:inputs instr))))
+       (first (bir:outputs instr))))
 
 (defmethod translate-simple-instruction ((inst cc-bmir:memref2) abi)
   (declare (ignore abi))
