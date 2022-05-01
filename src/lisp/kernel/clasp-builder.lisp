c0d30dba933330431883e689b283616ebbba5138
meister <meister@zeus.thirdlaw.tech>
2022-05-01 12:17:51 -0400

    Export core:load-aclasp

--- src/lisp/kernel/clasp-builder.lisp
+++ src/lisp/kernel/clasp-builder.lisp
@@ -697,7 +697,7 @@ Return files."
     (t (error "Unsupported value for core:*clasp-build-mode* -> ~a" core:*clasp-build-mode*))))
 
     
-(export '(compile-aclasp link-fasl))
+(export '(load-aclasp compile-aclasp link-fasl))
 (defun compile-aclasp (&key clean
                          (output-file (build-common-lisp-bitcode-pathname))
                          (target-backend (default-target-backend))
