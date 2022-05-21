c6d1ab85f89675ceb27c01b1ebb0d8eeef992cb7
Karsten Poeck <karsten.poeck@gmail.com>
2022-05-21 12:00:41 +0200

    Update diverse regression tests

--- src/lisp/regression-tests/set-unexpected-failures.lisp
+++ src/lisp/regression-tests/set-unexpected-failures.lisp
@@ -4,8 +4,12 @@
       '(loop-fixnum-1 loop-finally-1 loop-finally-2 loop-finally-3 loop-finally-4
         random-short random-double random-long
         test-issue-950
-        compile-file-no-unwind
+        ;; compile-file-no-unwind
         types-classes-10
         sbcl-cross-compile-4 ;;;not important
-        include-level-2a include-level-2b include-level-3 ;;; a problem for sbcl x-compiling
-        frame-function))
+        ;; include-level-2a
+        include-level-2b include-level-3 ;;; a problem for sbcl x-compiling
+        frame-function
+        ;; char-upcase.2a
+        ;; char-downcase.2a
+        ))
