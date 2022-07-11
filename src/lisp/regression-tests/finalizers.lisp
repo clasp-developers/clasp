6467a21833f266832ba9708a485c5a8019ef4278
Bike <aeshtaer@gmail.com>
2022-07-11 10:00:29 -0400

    Make finalizers tests a little more robust

--- src/lisp/regression-tests/finalizers.lisp
+++ src/lisp/regression-tests/finalizers.lisp
@@ -6,29 +6,37 @@
 
 ;;; ----------------------------------------------------------------------
 ;;;
-;;; Test finalizing CONS cells
-;;;
 ;;; This is a separate function in a perhaps-futile effort to prevent
 ;;; compiler optimizations from keeping the cons "reachable" when it's
 ;;; not in the source.
-;;; A better way to do this might involve weak pointers? FIXME
-(defun finalized-cons (len ninc)
-  (let ((a (make-list len))
+;;; Note that while we can use weak pointers to see if an object is accessible,
+;;; boehm won't actually run finalizers until some point after weak pointers
+;;; are splatted (according to gc.h) and other garbage collectors really
+;;; don't make many guarantees at all about if or when finalizers run.
+;;; So these tests is inherently dicey.
+
+(defun finalized-object (maker nfinalizers)
+  (let ((object (funcall maker))
         (count 0))
     (flet ((inc (a) (declare (ignore a)) (incf count)))
-      (loop repeat ninc do (gctools:finalize a #'inc))
-      (values a (lambda () count)))))
-(declaim (notinline finalized-cons))
+      (loop repeat nfinalizers do (gctools:finalize object #'inc))
+      (values (ext:make-weak-pointer object) (lambda () count)))))
+(declaim (notinline finalized-object))
+(defun test-finalizers (maker nfinalizers)
+  (multiple-value-bind (wp counter)
+      (finalized-object (lambda () (make-list 5)) 5)
+    ;; GC until the object becomes unreachable, and then
+    ;; try to force finalizers to be invoked for good measure.
+    ;; Max 10 iterations so we don't hang if something goes wrong.
+    (loop repeat 10
+          do (gctools:garbage-collect)
+          while (ext:weak-pointer-valid wp))
+    (gctools:invoke-finalizers)
+    (values (funcall counter) (ext:weak-pointer-valid wp))))
 
 (test finalizers-cons
-      (let ((count 0))
-        (let ((s (make-list 5)))
-          (flet ((inc (a) (declare (ignore a)) (incf count)))
-            (loop repeat 5 do (gctools:finalize s #'inc))))
-        ;; s is now unreachable
-        (loop repeat 10 do (gctools:garbage-collect))
-        count)
-      (5)
+      (test-finalizers (lambda () (make-list 5)) 5)
+      (5 nil)
       :description "Check if list of cons finalizers were executed")
 
 (test finalizers-cons-remove
@@ -42,18 +50,9 @@
       (0)
       :description "Check if list of cons finalizers were discarded")
 
-;;; ------------------------------------------------------------
-;;;
-;;; Test finalizing general objects
 (test finalizers-general
-      (let ((count 0))
-        (let ((s (make-array 5)))
-          (flet ((inc (a) (declare (ignore a)) (incf count)))
-            (loop repeat 5 do (gctools:finalize s #'inc))))
-        ;; S is now unreachable
-        (loop repeat 10 do (gctools:garbage-collect))
-        count)
-      (5)
+      (test-finalizers (lambda () (make-array 5)) 5)
+      (5 nil)
       :description "Check if list of general finalizers were executed")
 
 (test finalizers-general-remove
