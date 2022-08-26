b7a3c56d019a4c3bc61020c366500f778840c9e1
Bike <aeshtaer@gmail.com>
2022-08-26 14:45:20 -0400

    Move C++ bytecode compiler code into new files
    When we port the whole compiler to C++ it will be large, and
    there's no sense in keeping it all in bytecode.cc, which is
    already big with the interpreter.

--- src/core/bytecode.cc
+++ src/core/bytecode.cc
@@ -12,8 +12,6 @@
 #include <clasp/core/array.h>
 #include <clasp/core/virtualMachine.h>
 #include <clasp/core/primitives.h> // cl__fdefinition
-#include <clasp/core/evaluator.h> // af_interpreter_lookup_macro
-#include <clasp/core/sysprop.h> // core__get_sysprop
 #include <clasp/core/unwind.h>
 #include <clasp/core/ql.h>
 
@@ -67,51 +65,6 @@ T_sp BytecodeModule_O::compileInfo() const {
 CL_DEFMETHOD
 void BytecodeModule_O::setf_compileInfo(T_sp o) {
   this->_CompileInfo = o;
-}
-
-T_sp BytecodeCmpEnv_O::variableInfo(T_sp varname) {
-  T_sp vars = this->vars();
-  if (vars.nilp()) return vars;
-  else {
-    T_sp pair = gc::As<Cons_sp>(vars)->assoc(varname, nil<T_O>(),
-                                             cl::_sym_eq, nil<T_O>());
-    if (pair.nilp()) return pair;
-    else return oCdr(pair);
-  }
-}
-
-T_sp BytecodeCmpEnv_O::lookupSymbolMacro(T_sp sname) {
-  T_sp info = this->variableInfo(sname);
-  if (gc::IsA<BytecodeCmpSymbolMacroVarInfo_sp>(info))
-    return gc::As_unsafe<BytecodeCmpSymbolMacroVarInfo_sp>(info)->expander();
-  else if (info.notnilp()) { // global?
-    T_mv result = core__get_sysprop(sname, ext::_sym_symbolMacro);
-    if (gc::As<T_sp>(result.valueGet_(1)).notnilp()) {
-      return result;
-    } else return nil<T_O>();
-  } else return nil<T_O>();
-}
-
-T_sp BytecodeCmpEnv_O::functionInfo(T_sp funname) {
-  T_sp funs = this->funs();
-  if (funs.nilp()) return funs;
-  else {
-    T_sp pair = gc::As<Cons_sp>(funs)->assoc(funname, nil<T_O>(),
-                                             cl::_sym_equal, nil<T_O>());
-    if (pair.nilp()) return pair;
-    else return oCdr(pair);
-  }
-}
-
-T_sp BytecodeCmpEnv_O::lookupMacro(T_sp macroname) {
-  T_sp info = this->functionInfo(macroname);
-  if (gc::IsA<BytecodeCmpGlobalMacroInfo_sp>(info))
-    return gc::As_unsafe<BytecodeCmpGlobalMacroInfo_sp>(info)->expander();
-  else if (gc::IsA<BytecodeCmpLocalMacroInfo_sp>(info))
-    return gc::As_unsafe<BytecodeCmpLocalMacroInfo_sp>(info)->expander();
-  else if (info.nilp()) // could be global
-    return af_interpreter_lookup_macro(macroname, nil<T_O>());
-  else return nil<T_O>();
 }
 
 static inline uint8_t read_uint8( unsigned char*& pc ) {
