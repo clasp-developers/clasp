af966f92695a9ce16e33715a838b1c20ac2d1452
meister <meister@zeus.thirdlaw.tech>
2022-09-08 00:39:16 -0400

    More stuff for lframe extension function

--- debugger-tools/udb-loader.py
+++ debugger-tools/udb-loader.py
@@ -51,6 +51,15 @@ class LispInspect (gdb.Command):
     maybeReloadModules()
     inspector_mod.do_lisp_inspect(debugger_mod,arg)
     
+class LispFrame (gdb.Command):
+  def __init__ (self):
+    super (LispFrame, self).__init__ ("lframe", gdb.COMMAND_USER)
+
+  def invoke (self, arg, from_tty):
+    global inspector_mod, debugger_mod
+    maybeReloadModules()
+    inspector_mod.do_lisp_frame(debugger_mod,arg)
+    
 class LispTest (gdb.Command):
   def __init__ (self):
     super (LispTest, self).__init__ ("ltest", gdb.COMMAND_USER)
@@ -73,11 +82,13 @@ LispInspect()
 LispPrint()
 LispHead()
 LispTest()
+LispFrame()
 LispVm()
 
 print("lprint <address>   - print lisp object in compact form")
 print("linspect <address> - inspect lisp object - all fields")
 print("lhead <address>    - dump the clients header")
+print("lframe             - Dump the function name and args for a lisp frame trampoline"
 print("ltest <address>    - test module reloading")
 print("lvm                - Dump current vm status")
 print("python-interactive <expr> - (or pi) interactive Python session\n")
