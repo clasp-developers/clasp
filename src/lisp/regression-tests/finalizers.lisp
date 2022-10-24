4e642e48efaa5af2b19b8bcb122d15262cf28e89
Bike <aeshtaer@gmail.com>
2022-10-24 11:48:37 -0400

    Increase reliability of finalizers tests
    Trying to finalize a bunch of objects once rather than one object
    a bunch of times seems to work better, given that finalizers
    running at all is not really guaranteed ever.

--- src/lisp/regression-tests/finalizers.lisp
+++ src/lisp/regression-tests/finalizers.lisp
@@ -13,30 +13,43 @@
 ;;; boehm won't actually run finalizers until some point after weak pointers
 ;;; are splatted (according to gc.h) and other garbage collectors really
 ;;; don't make many guarantees at all about if or when finalizers run.
-;;; So these tests is inherently dicey.
+;;; So these tests are inherently dicey.
 
-(defun finalized-object (maker nfinalizers)
-  (let ((object (funcall maker))
-        (count 0))
+(defun finalized-objects (maker n)
+  (let ((count 0))
+    ;; FIXME: Maybe should be atomic-incf, depending on how our GC
+    ;; runs finalizers. But that would require atomics support for lexicals.
     (flet ((inc (a) (declare (ignore a)) (incf count)))
-      (loop repeat nfinalizers do (gctools:finalize object #'inc))
-      (values (ext:make-weak-pointer object) (lambda () count)))))
-(declaim (notinline finalized-object))
-(defun test-finalizers (maker nfinalizers)
-  (multiple-value-bind (wp counter)
-      (finalized-object (lambda () (make-list 5)) 5)
-    ;; GC until the object becomes unreachable, and then
+      (values (loop repeat n
+                    for object = (funcall maker)
+                    do (gctools:finalize object #'inc)
+                    collect (ext:make-weak-pointer object))
+              (lambda () count)))))
+(declaim (notinline finalized-objects))
+
+(defun test-finalizers (maker n)
+  (multiple-value-bind (wps counter)
+      (finalized-objects maker n)
+    ;; Try to GC until the objects become unreachable, and then
     ;; try to force finalizers to be invoked for good measure.
     ;; Max 10 iterations so we don't hang if something goes wrong.
     (loop repeat 10
-          do (gctools:garbage-collect)
-          while (ext:weak-pointer-valid wp))
+          do (gctools:garbage-collect))
     (gctools:invoke-finalizers)
-    (values (funcall counter) (ext:weak-pointer-valid wp))))
+    ;; Since finalizers are inherently a little unreliable, we just check
+    ;; that the count of ran finalizers and uncollected objects sums to n
+    ;; (i.e. no object was finalized that is still accessible), and that
+    ;; at least 95% of the finalizers ran. This idea is cribbed from SBCL.
+    ;; SBCL also displays traces for any unsplatted objects, which might be
+    ;; nice to do if we ever grow that capability.
+    (let* ((count (funcall counter))
+           (success-fraction (/ count n)))
+      (values (or (> success-fraction (/ 95 100)) success-fraction)
+              (+ count (count-if #'ext:weak-pointer-valid wps))))))
 
 (test finalizers-cons
-      (test-finalizers (lambda () (make-list 5)) 5)
-      (5 nil)
+      (test-finalizers (lambda () (make-list 5)) 100)
+      (t 100)
       :description "Check if list of cons finalizers were executed")
 
 (test finalizers-cons-remove
@@ -51,8 +64,8 @@
       :description "Check if list of cons finalizers were discarded")
 
 (test finalizers-general
-      (test-finalizers (lambda () (make-array 5)) 5)
-      (5 nil)
+      (test-finalizers (lambda () (make-array 5)) 100)
+      (t 100)
       :description "Check if list of general finalizers were executed")
 
 (test finalizers-general-remove
