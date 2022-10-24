9dd368dca5af51217a46b247311377a40846d86f
Bike <aeshtaer@gmail.com>
2022-10-24 12:23:14 -0400

    Remove some expected failures from the tests
    RANDOM-SHORT etc fail on mac but not linux.

--- src/lisp/regression-tests/set-unexpected-failures.lisp
+++ src/lisp/regression-tests/set-unexpected-failures.lisp
@@ -3,13 +3,9 @@
 (setq *expected-failures*
       '(loop-fixnum-1 loop-finally-1 loop-finally-2 loop-finally-3 loop-finally-4
         random-short random-double random-long
-        test-issue-950
         ;; compile-file-no-unwind
         types-classes-10
         sbcl-cross-compile-4 ;;;not important
         ;; include-level-2a
         include-level-2b include-level-3 ;;; a problem for sbcl x-compiling
-        frame-function
-        ;; char-upcase.2a
-        ;; char-downcase.2a
-        FINALIZERS-GENERAL PRINT-6))
+        frame-function))
