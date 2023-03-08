c3a43c4775e6c2b6172b10b25b1337f6f53636df
Bike <aeshtaer@gmail.com>
2023-03-08 13:49:06 -0500

    Update intrinsic mangling for llvm15

--- src/lisp/kernel/cleavir/translate.lisp
+++ src/lisp/kernel/cleavir/translate.lisp
@@ -411,7 +411,7 @@
                    come-from *clasp-system*))
          (frame
            (unless simplep
-             (%intrinsic-call "llvm.frameaddress.p0i8"
+             (%intrinsic-call "llvm.frameaddress.p0"
                               (list (%i32 0)))))
          (normal-successor (first successors))
          (bufp (cmp:alloca cmp::%jmp-buf-tag% 1 "come-from-jmp-buf")))
@@ -613,7 +613,7 @@
                              'insert-step-conditions)
     (let ((origin (bir:origin inst)))
       (when (typep origin 'cst:cst)
-        (let* ((frame (%intrinsic-call "llvm.frameaddress.p0i8"
+        (let* ((frame (%intrinsic-call "llvm.frameaddress.p0"
                                        (list (%i32 0)) "stepper-frame"))
                (raw (cst:raw origin))
                ;; See #1376: Sometimes the source form will be a literal.
@@ -647,7 +647,7 @@
     ;; so now we need to put in the cc_breakstep_after to support
     ;; the step-over facility.
     (%intrinsic-call "cc_breakstep_after"
-                     (list (%intrinsic-call "llvm.frameaddress.p0i8"
+                     (list (%intrinsic-call "llvm.frameaddress.p0"
                                             (list (%i32 0))
                                             "stepper-frame")))))
 
@@ -1555,7 +1555,7 @@
                                   "adjusted-nret-variable"))
                (ncopy (cmp:irc-sub adjusted-nvalues (%size_t 1) "ntocopy")))
           ;; Copy the rest
-          (%intrinsic-call "llvm.memcpy.p0i8.p0i8.i64"
+          (%intrinsic-call "llvm.memcpy.p0.p0.i64"
                            (list (cmp:irc-bit-cast dest cmp:%i8*%
                                                    "var-dest-subsequent")
                                  ;; read from the 1st value of the mv vector
@@ -1576,7 +1576,7 @@
           for dest = (cmp:irc-typed-gep-variable cmp:%t*% valvec (list startn) "dest")
           do (ecase key
                ((:saved)
-                (%intrinsic-call "llvm.memcpy.p0i8.p0i8.i64"
+                (%intrinsic-call "llvm.memcpy.p0.p0.i64"
                                  (list (cmp:irc-bit-cast dest cmp:%i8*% "dest")
                                        (cmp:irc-bit-cast extra cmp:%i8*%
                                                          "source")
