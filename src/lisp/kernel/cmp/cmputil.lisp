76d3f8700dc4bac7d8f65b77612da75073336b1f
Bike <aeshtaer@gmail.com>
2023-06-16 11:44:40 -0400

    Delete old bclasp code

--- src/lisp/kernel/cmp/cmputil.lisp
+++ src/lisp/kernel/cmp/cmputil.lisp
@@ -33,7 +33,6 @@
 
 (defvar *global-function-defs*)
 (defvar *global-function-refs*)
-(defvar *compilation-messages*)
 
 (defvar *warnings-p*)
 (defvar *failure-p*)
@@ -45,102 +44,6 @@
   '((compilation-speed 1)
     (debug 1) (space 1) (speed 1) (safety 1)))
 (defvar *policy* ())
-
-(core:defconstant-equal +note-format+        "Note:          {}")
-(core:defconstant-equal +style-warn-format+  "Style warning: {}")
-(core:defconstant-equal +warn-format+        "Warning:       {}")
-(core:defconstant-equal +error-format+       "Error:         {}")
-(core:defconstant-equal +fatal-format+       "Fatal-error:   {}")
-
-(defstruct (compiler-message (:type vector))
-  (format +note-format+)
-  message
-  source-pos-info)
-
-(defstruct (compiler-note (:include compiler-message)
-                          (:type vector)))
-
-(defstruct (compiler-warning
-             (:type vector)
-             (:include compiler-message
-              (format +warn-format+))))
-
-(defstruct (compiler-error
-             (:type vector)
-             (:include compiler-message
-              (format +error-format+))))
-
-(defstruct (compiler-fatal-error
-             (:type vector)
-             (:include compiler-error
-              (format +fatal-format+))))
-
-(defstruct (compiler-style-warning
-             (:type vector)
-             (:include compiler-warning
-              (format +style-warn-format+))))
-
-;;; defined analogously to WARN, etc.
-(defun coerce-compiler-message (datum args)
-  (cond ((stringp datum) ; it's a fmt string
-         (apply #'core:fmt nil datum args))
-        ((null args) ; it's some kind of object, e.g. a condition
-         (princ-to-string datum))
-        (t (error "BUG: Bad arguments to compiler-warn/error/something: ~a ~a"
-                  datum args))))
-
-(defun compiler-error (spi datum &rest args)
-  (setf *failure-p* (setf *warnings-p* t))
-  (let* ((cspi (or spi (ext:current-source-location)))
-         (err (make-compiler-error :message (coerce-compiler-message datum args)
-                                   :source-pos-info cspi)))
-      (push err *compilation-messages*)))
-
-(defun compiler-warn (spi datum &rest args)
-  (setf *failure-p* (setf *warnings-p* t))
-  (let* ((cspi (or spi (ext:current-source-location)))
-         (err (make-compiler-warning :message (coerce-compiler-message datum args)
-                                     :source-pos-info cspi)))
-    (push err *compilation-messages*)))
-
-;;; We redefine all these warn-specific-thing functions later to signal actual
-;;; conditions using actual format strings.
-
-(defun warn-undefined-global-variable (spi var)
-  (compiler-warn spi "Undefined variable {}" var))
-
-(defun warn-undefined-type (spi type)
-  (compiler-style-warn spi "Undefined type {}" type))
-
-(defun warn-cannot-coerce (spi type)
-  (compiler-style-warn spi "Cannot coerce to type {}: unknown or not defined for coerce" type))
-
-(defun warn-invalid-number-type (spi type)
-  (compiler-warn spi "Invalid number type: {}" type))
-
-(defun warn-icsp-iesp-both-specified (spi)
-  (compiler-warn
-   spi ":initial-contents and :initial-element both specified"))
-
-(defun compiler-style-warn (spi datum &rest args)
-  (setf *warnings-p* t)
-  (let* ((cspi (or spi (ext:current-source-location)))
-         (err (make-compiler-style-warning :message (coerce-compiler-message datum args)
-                                           :source-pos-info cspi)))
-    (push err *compilation-messages*)))
-
-(defun describe-source-location (spi)
-  (let* ((lineno (source-pos-info-lineno spi))
-         (filepos (source-pos-info-filepos spi))
-         (file-scope (file-scope spi))
-         (pathname (file-scope-pathname file-scope)))
-  (core:fmt nil "{} filepos: {}  lineno: {}" (namestring pathname) filepos lineno)))
-
-(defun print-compiler-message (c stream)
-  (let ((msg (core:fmt nil (compiler-message-format c) (compiler-message-message c))))
-    (fresh-line stream)
-    (core:fmt stream ";;; {}%N" msg)
-    (core:fmt stream ";;;     at {} %N" (describe-source-location (compiler-message-source-pos-info c)))))
 
 ;;; (setq core::*echo-repl-read* t)
 
@@ -167,39 +70,10 @@
 
 (defvar *active-protection* nil)
 
-;;; Will be redefined in compiler-conditions.lisp
-(defun do-compilation-unit (closure &key override)
-  (if (or (not *active-protection*) ; we're not in a do-compilation-unit
-          override) ; we are, but we're overriding it
-      (let* ((*active-protection* t)
-             (*compilation-messages* nil)
-             (*global-function-defs* (make-global-function-defs-table))
-             (*global-function-refs* (make-global-function-refs-table)))
-        (unwind-protect
-             (funcall closure) ; --> result*
-          (compilation-unit-finished *compilation-messages*)))
-      (funcall closure)))
-
-;;; Redefined in compiler-conditions.lisp
-(defun compilation-unit-finished (messages)
-  ;; Add messages for global function references that were never satisfied
-  (maphash (lambda (name references)
-             (unless (or (fboundp name)
-                         (gethash name *global-function-defs*))
-               (dolist (ref references)
-                 (pushnew (make-compiler-style-warning
-                           :message (core:fmt nil "Undefined function {}" name)
-                           :source-pos-info (global-function-ref-source-pos-info ref))
-                          messages :test #'equalp))))
-           *global-function-refs*)
-  (dolist (m (reverse messages))
-    (print-compiler-message m *error-output*)))
-
 (defstruct (global-function-def (:type vector) :named)
   type
   name
   source-pos-info)
-
 
 (defstruct (global-function-ref (:type vector) :named)
   name
@@ -210,12 +84,6 @@
        (gethash name *global-function-defs*)))
 
 (export '(known-function-p)) ; FIXME MOVE
-
-;;; Redefined in compiler-conditions.lisp
-(defun warn-redefined-function (name new-type new-origin old-type old-origin)
-  (compiler-warn new-origin "The {} {} was previously defined as a {} at {}%N"
-                 new-type name
-                 old-type (describe-source-location old-origin)))
 
 (defun register-global-function-def (type name)
   (when (boundp '*global-function-defs*)
@@ -251,13 +119,6 @@
 
 (defmacro with-compilation-results ((&rest options) &body body)
   `(call-with-compilation-results (lambda () ,@body) ,@options))
-
-;;; Redefined in compiler-conditions.lisp
-(defun call-with-compilation-results (thunk &rest options)
-  (declare (ignore options)) ; maybe later
-  (let ((*warnings-p* nil)
-        (*failure-p* nil))
-    (values (funcall thunk) *warnings-p* *failure-p*)))
 
 (export 'with-atomic-file-rename)
 (defmacro with-atomic-file-rename ((temp-pathname final-pathname) &body body)
