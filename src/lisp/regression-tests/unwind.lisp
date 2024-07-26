a5d224d126adbc88c7efc084a8f13616ada3fccb
kalinda <kalinda@zeus.thirdlaw.tech>
2024-07-26 13:26:14 -0400

    package tests fixed

--- src/lisp/regression-tests/unwind.lisp
+++ src/lisp/regression-tests/unwind.lisp
@@ -1,19 +1,21 @@
 (in-package #:clasp-tests)
 
-;; should probably not do this i just got frustrated
-(core:package-unlock "CORE")
-(core:package-unlock "CL")
-;; Assert that this compilation does not invoke any unwinds.
-(test compile-file-no-unwind
-      (let ((unwinds (gctools:thread-local-unwinds)))
-        (compile-file "sys:src;lisp;kernel;lsp;predlib.lisp"
-                      :execution :serial
-                      :output-file (make-pathname
-                                    :type (pathname-type (compile-file-pathname "foo.lisp"))
-                                    :defaults (core:mkstemp "/tmp/predlib")))
-        (- (gctools:thread-local-unwinds) unwinds)
-        )
-      (0)
+(let ((lockedcl (si::package-is-locked "COMMON-LISP")) (lockedcore (si::package-is-locked "CORE")))
+  (core:package-unlock "CORE")
+  (core:package-unlock "CL")
+  ;; Assert that this compilation does not invoke any unwinds.
+  (test compile-file-no-unwind
+        (let ((unwinds (gctools:thread-local-unwinds)))
+          (compile-file "sys:src;lisp;kernel;lsp;predlib.lisp"
+                        :execution :serial
+                        :output-file (make-pathname
+                                      :type (pathname-type (compile-file-pathname "foo.lisp"))
+                                      :defaults (core:mkstemp "/tmp/predlib")))
+          (- (gctools:thread-local-unwinds) unwinds)
+          )
+        (0)
+  )
+
+  (if lockedcl (si::package-lock "COMMON-LISP"))
+  (if lockedcore (si::package-lock "CORE"))
 )
-(core:package-lock "CORE")
-(core:package-lock "CL")
