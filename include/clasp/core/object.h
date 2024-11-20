6bef0d399726146f37127032400dde9e86a298e1
Bike <aeshtaer@gmail.com>
2024-11-20 12:44:03 -0500

    In snapshot load, don't bother with constructor
    I don't see any reason not to just use fill directly. The commit
    history doesn't make the justification clear. If it's supposed to
    be for correctness, I don't see anything in cppreference that
    would make going through a constructor more correct. I think the
    correct thing would be for all objects to be trivially copyable,
    but I don't think it's practically a problem even if they aren't.
    (Which is good, because we have nontrivial destructors.)

--- include/clasp/core/object.h
+++ include/clasp/core/object.h
@@ -314,7 +314,6 @@ public:                                                                         
   static core::Symbol_sp static_classSymbol() { return core::lisp_getStaticClassSymbol(oClass::static_ValueStampWtagMtag); };      \
   static string Package() { return oClass::static_packageName(); };                                                                \
   static string Pkg() { return Package(); };                                                                                       \
-  oClass(snapshotSaveLoad::snapshot_save_load_init_s* isl) { isl->fill((void*)this); };                                            \
   static void expose_to_clasp();
 
 #define LISP_TEMPLATE_CLASS(oClass) COMMON_CLASS_PARTS(CurrentPkg, oClass, typeid(oClass).name())
