b431bcc54d68ac8842f45113079ef3dc33f45803
Bike <aeshtaer@gmail.com>
2025-01-16 16:59:04 -0500

    Teach static analyzer about weak pointers
    Not useful yet but it took me a while to figure it out

--- include/clasp/gctools/gc_boot.h
+++ include/clasp/gctools/gc_boot.h
@@ -34,6 +34,7 @@ enum Data_types {
   SMART_PTR_OFFSET,
   ATOMIC_SMART_PTR_OFFSET,
   TAGGED_POINTER_OFFSET,
+  WEAK_PTR_OFFSET,
   RAW_POINTER_OFFSET,
   ARRAY_OFFSET,
   POINTER_OFFSET,
@@ -75,7 +76,7 @@ enum Data_types {
 //
 // Use powers of two for the flags
 //
-enum ClassFlags { IS_POLYMORPHIC = 1 };
+enum ClassFlags { IS_POLYMORPHIC = 1, COMPLEX_SCAN = 2 };
 
 extern uintptr_t global_lisp_kind;
 extern uintptr_t global_cons_kind;
