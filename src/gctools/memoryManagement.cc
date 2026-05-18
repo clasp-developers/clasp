bb1e742f59bcd65231656b9b418318afd7c2ea55
Bike <aeshtaer@gmail.com>
2026-05-18 10:52:00 -0400

    start tracing stacks
    doesn't seem to be working yet

--- src/gctools/memoryManagement.cc
+++ src/gctools/memoryManagement.cc
@@ -458,11 +458,44 @@ namespace gctools {
 
 /* Walk all of the global (not per-thread) roots,
  * passing the address of each root */
-template <typename RootWalkCallback>
+template <std::invocable<Tagged*> RootWalkCallback>
 void walkGlobalRoots(RootWalkCallback&& callback) {
   // luckily, just the one god object references everything else
   callback((Tagged*)&_lisp);
 };
+
+template <std::invocable<Tagged*> ThreadWalkCallback>
+void walkThreadRoots(ThreadWalkCallback&& callback) {
+  // NOTE we don't need the threads mutex since we must have stopped the world.
+  for (auto cur : _lisp->_Roots._ActiveThreads) {
+    mp::Process_sp proc = core::oCar(cur).as_assert<mp::Process_O>();
+    core::ThreadLocalState* tls = proc->_ThreadInfo;
+    tls->walkRoots(callback);
+    tls->walkVMStack(callback);
+    tls->walkControlStack([&](Tagged* tp) {
+      // The control stack we have to walk conservatively.
+      switch(ptag(*tp)) {
+      case general_tag: {
+        Header_s* header = (Header_s*)GeneralPtrToHeaderPtr(untag_object((void*)*tp));
+        if (header->isValidGeneralObject())
+          callback(tp);
+      } break;
+      case cons_tag: {
+        ConsHeader_s* header = (ConsHeader_s*)ConsPtrToHeaderPtr(untag_object((void*)*tp));
+        if (header->isValidConsObject())
+          callback(tp);
+      } break;
+      default: callback(tp);
+      }
+    });
+  }
+}
+
+template <std::invocable<Tagged*> WalkCallback>
+void walkRoots(WalkCallback&& callback) {
+  walkGlobalRoots(callback);
+  walkThreadRoots(callback);
+}
 
 static void mw_obj_scan(core::General_O* client,
                         std::stack<std::pair<Tagged, Tagged>>& markStack) {
@@ -668,7 +701,7 @@ void traceablep(std::unordered_map<Tagged, bool>& testing) {
       // so leave us in the set.
     };
 
-  walkGlobalRoots([&](Tagged* rootf) { markStack.push(*rootf); });
+  walkRoots([&](Tagged* rootf) { markStack.push(*rootf); });
 
  trace:
   while (!markStack.empty()) {
