cfb8b1ce2e2251a7e5e3f2e4ce35265401842a5e
Bike <aeshtaer@gmail.com>
2026-08-29 01:24:27 -0400

    mmtk: only read TLS once per allocation (hopefully)
    thread local storage is expensive
    
    every day i'm microoptimizn'

--- include/clasp/gctools/gcalloc_mmtk.h
+++ include/clasp/gctools/gcalloc_mmtk.h
@@ -11,49 +11,55 @@
 
 namespace gctools {
 
-inline void* mmtk_alloc_raw(size_t size, MMTkClaspAllocSemantics semantics) {
-  return mmtk_clasp_alloc(my_thread_low_level->_mmtk_mutator, size, CLASP_ALIGNMENT, semantics);
+inline void* mmtk_alloc_raw(ThreadLocalStateLowLevel* thread_ll,
+                            size_t size, MMTkClaspAllocSemantics semantics) {
+  return mmtk_clasp_alloc(thread_ll->_mmtk_mutator,
+                          size, CLASP_ALIGNMENT, semantics);
 }
 
-inline void mmtk_post_alloc(void* alloc_start, size_t size, MMTkClaspAllocSemantics semantics,
+inline void mmtk_post_alloc(ThreadLocalStateLowLevel* thread_ll,
+                            void* alloc_start, size_t size, MMTkClaspAllocSemantics semantics,
                              size_t header_size = sizeof(Header_s)) {
   void* client = reinterpret_cast<char*>(alloc_start) + header_size;
-  mmtk_clasp_post_alloc(my_thread_low_level->_mmtk_mutator, client, size, semantics);
+  mmtk_clasp_post_alloc(thread_ll->_mmtk_mutator, client, size, semantics);
 }
 
 // --- Cons allocation ---
 
 template <typename Stage, typename Cons, size_t Size>
 inline ConsHeader_s* do_cons_allocation() {
-  RAIIDisableInterrupts disable_interrupts;
+  // TLS access is expensive so we want to do as little of it as we can.
+  ThreadLocalStateLowLevel* thread_ll = my_thread_low_level;
+  RAIIDisableInterrupts disable_interrupts(thread_ll);
   void* alloc_start;
-  alloc_start = mmtk_alloc_raw(Size, MMTK_CLASP_ALLOC_DEFAULT);
+  alloc_start = mmtk_alloc_raw(thread_ll, Size, MMTK_CLASP_ALLOC_DEFAULT);
   ConsHeader_s* header = reinterpret_cast<ConsHeader_s*>(alloc_start);
   const ConsHeader_s::StampWtagMtag stamp(ConsHeader_s::BadgeStampWtagMtag::make<Cons>());
   new (header) ConsHeader_s(stamp);
-  my_thread_low_level->_Allocations.registerAllocation(STAMPWTAG_CONS, Size);
-  mmtk_post_alloc(alloc_start, Size, MMTK_CLASP_ALLOC_DEFAULT, sizeof(ConsHeader_s));
+  thread_ll->_Allocations.registerAllocation(STAMPWTAG_CONS, Size);
+  mmtk_post_alloc(thread_ll, alloc_start, Size, MMTK_CLASP_ALLOC_DEFAULT, sizeof(ConsHeader_s));
   return header;
 }
 
 static inline Header_s* do_generic_slow_allocation(const Header_s::StampWtagMtag& the_header, size_t size, MMTkClaspAllocSemantics semantics) {
-  RAIIDisableInterrupts disable_interrupts;
+  ThreadLocalStateLowLevel* thread_ll = my_thread_low_level;
+  RAIIDisableInterrupts disable_interrupts(thread_ll);
   size_t true_size = size;
 #ifdef DEBUG_GUARD
   size_t tail_size = ((rand() % 8) + 1) * Alignment();
   true_size += tail_size;
 #endif
   void* alloc_start;
-  alloc_start = mmtk_alloc_raw(true_size, semantics);
+  alloc_start = mmtk_alloc_raw(thread_ll, true_size, semantics);
   Header_s* header = reinterpret_cast<Header_s*>(alloc_start);
-  my_thread_low_level->_Allocations.registerAllocation(the_header.unshifted_stamp(), true_size);
+  thread_ll->_Allocations.registerAllocation(the_header.unshifted_stamp(), true_size);
 #ifdef DEBUG_GUARD
   memset(header, 0x00, true_size);
   new (header) Header_s(the_header, size, tail_size, true_size);
 #else
   new (header) Header_s(the_header);
 #endif
-  mmtk_post_alloc(alloc_start, true_size, semantics);
+  mmtk_post_alloc(thread_ll, alloc_start, true_size, semantics);
   return header;
 }
 
@@ -63,13 +69,14 @@ static inline Header_s* do_generic_slow_allocation(const Header_s::StampWtagMtag
   // size is variable, give up
   return do_generic_slow_allocation(the_header, Size, semantics);
 #else
-  RAIIDisableInterrupts disable_interrupts;
+  ThreadLocalStateLowLevel* thread_ll = my_thread_low_level;
+  RAIIDisableInterrupts disable_interrupts(thread_ll);
   void* alloc_start;
-  alloc_start = mmtk_alloc_raw(Size, semantics);
+  alloc_start = mmtk_alloc_raw(thread_ll, Size, semantics);
   Header_s* header = reinterpret_cast<Header_s*>(alloc_start);
-  my_thread_low_level->_Allocations.registerAllocation(the_header.unshifted_stamp(), Size);
+  thread_ll->_Allocations.registerAllocation(the_header.unshifted_stamp(), Size);
   new (header) Header_s(the_header);
-  mmtk_post_alloc(alloc_start, Size, semantics);
+  mmtk_post_alloc(thread_ll, alloc_start, Size, semantics);
   return header;
 #endif
 }
